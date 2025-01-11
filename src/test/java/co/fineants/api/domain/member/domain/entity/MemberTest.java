3cf6ebcd0b2dcd396173b4b394b3db5a72a55089
yonghwankim-dev <dragonbead95@naver.com>
2025-01-11 14:10:45 +0900

    test: 알림 개인설정 설정 테스트

--- src/test/java/co/fineants/api/domain/member/domain/entity/MemberTest.java
+++ src/test/java/co/fineants/api/domain/member/domain/entity/MemberTest.java
@@ -5,17 +5,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import co.fineants.TestDataFactory;
+import co.fineants.api.domain.notificationpreference.domain.entity.NotificationPreference;
 
 class MemberTest {
 
 	@DisplayName("회원에 매니저 역할을 추가한다")
 	@Test
-	void addMemberRole() {
+	void givenMember_whenAddMemberRole_thenAddRoleList() {
 		// given
 		Member member = TestDataFactory.createMember();
-		Role memberRole = Role.create("ROLE_USER", "회원");
+		Role userRole = Role.create("ROLE_USER", "회원");
 		Role managerRole = Role.create("ROLE_MANAGER", "매니저");
-		MemberRole memberMemberRole = MemberRole.of(member, memberRole);
+		MemberRole memberMemberRole = MemberRole.of(member, userRole);
 		MemberRole managerMemberRole = MemberRole.of(member, managerRole);
 		// when
 		member.addMemberRole(memberMemberRole, managerMemberRole);
@@ -24,43 +25,45 @@ class MemberTest {
 			.hasSize(2)
 			.containsExactlyInAnyOrder(memberMemberRole, managerMemberRole);
 	}
-	//
-	// @DisplayName("MemberRole을 다른 회원의 역할에 추가하면 기존 연관관계를 해제한다")
-	// @Test
-	// void addMemberRole_whenAssignMemberRoleToOtherMember_thenReleaseEntityRelationShip() {
-	// 	// given
-	// 	Member member = createMember();
-	// 	Role memberRole = Role.create("ROLE_USER", "회원");
-	// 	Role managerRole = Role.create("ROLE_MANAGER", "매니저");
-	// 	MemberRole managerMemberRole = MemberRole.of(member, managerRole);
-	// 	member.addMemberRole(managerMemberRole);
-	//
-	// 	Member otherMember = createMember("other1", "other1@gmail.com");
-	// 	// when
-	// 	otherMember.addMemberRole(managerMemberRole);
-	// 	// then
-	// 	Assertions.assertThat(member.getRoles())
-	// 		.hasSize(1)
-	// 		.containsExactly(MemberRole.of(member, memberRole));
-	// 	Assertions.assertThat(otherMember.getRoles())
-	// 		.hasSize(2)
-	// 		.containsExactlyInAnyOrder(MemberRole.of(otherMember, memberRole), MemberRole.of(otherMember, managerRole));
-	// }
-	//
-	// @Transactional
-	// @DisplayName("회원의 알림 설정을 변경한다")
-	// @Test
-	// void setNotificationPreference() {
-	// 	// given
-	// 	Member member = createMember();
-	// 	NotificationPreference preference = NotificationPreference.create(false, false, false, false);
-	// 	// when
-	// 	member.setNotificationPreference(preference);
-	// 	// then
-	// 	Assertions.assertThat(member.getNotificationPreference()).isEqualTo(preference);
-	// 	Assertions.assertThat(preference.getMember()).isEqualTo(member);
-	// }
-	//
+
+	@DisplayName("MemberRole을 다른 회원의 역할에 추가하면 기존 연관관계를 해제한다")
+	@Test
+	void addMemberRole_whenAssignMemberRoleToOtherMember_thenReleaseEntityRelationShip() {
+		// given
+		Member member = TestDataFactory.createMember();
+		Role userRole = Role.create("ROLE_USER", "회원");
+		Role managerRole = Role.create("ROLE_MANAGER", "매니저");
+		MemberRole userMemberRole = MemberRole.of(member, userRole);
+		MemberRole managerMemberRole = MemberRole.of(member, managerRole);
+		member.addMemberRole(userMemberRole, managerMemberRole);
+
+		Member otherMember = TestDataFactory.createMember("other1", "other1@gmail.com");
+		otherMember.addMemberRole(MemberRole.of(otherMember, userRole));
+		// when
+		otherMember.addMemberRole(managerMemberRole);
+		// then
+		Assertions.assertThat(member.getRoles())
+			.hasSize(1)
+			.containsExactly(MemberRole.of(member, userRole));
+		Assertions.assertThat(otherMember.getRoles())
+			.hasSize(2)
+			.containsExactlyInAnyOrder(MemberRole.of(otherMember, userRole), MemberRole.of(otherMember, managerRole));
+	}
+
+	@DisplayName("회원의 알림 설정을 전부 활성화로 변경한다")
+	@Test
+	void givenMember_whenSetNotificationPreference_thenNotificationPreferenceIsAllActive() {
+		// given
+		Member member = TestDataFactory.createMember();
+		NotificationPreference preference = NotificationPreference.allActive();
+		// when
+		member.setNotificationPreference(preference);
+		// then
+		NotificationPreference expected = NotificationPreference.allActive();
+		Assertions.assertThat(member.getNotificationPreference()).isEqualTo(expected);
+		Assertions.assertThat(preference.getMember()).isEqualTo(member);
+	}
+
 	// @DisplayName("알림이 회원을 변경한다")
 	// @Test
 	// void setMember() {
