c8eb7ee01a8ffb3bcb283c1f853f6eefdc167bd1
yonghwankim-dev <dragonbead95@naver.com>
2025-01-15 12:39:03 +0900

    test: 검증코드 생성기 테스트에서 스프링 컨텍스트 제거

--- src/test/java/co/fineants/api/domain/member/service/VerifyCodeGeneratorTest.java
+++ src/test/java/co/fineants/api/domain/member/service/VerifyCodeGeneratorTest.java
@@ -2,25 +2,25 @@ package co.fineants.api.domain.member.service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import co.fineants.AbstractContainerBaseTest;
+class VerifyCodeGeneratorTest {
 
-class VerifyCodeGeneratorTest extends AbstractContainerBaseTest {
-
-	@Autowired
 	private VerifyCodeGenerator verifyCodeGenerator;
+
+	@BeforeEach
+	void setUp() {
+		verifyCodeGenerator = new VerifyCodeGenerator(6, 1000000);
+	}
 
 	@DisplayName("사용자는 검증코드를 생성한다")
 	@Test
 	void generate() {
 		// given
-
 		// when
 		String result = verifyCodeGenerator.generate();
-
 		// then
 		assertThat(result).hasSize(6);
 	}
