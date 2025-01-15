479969cad249c5d05b19273c41d4959a01182816
yonghwankim-dev <dragonbead95@naver.com>
2025-01-15 12:37:40 +0900

    test: 닉네임 생성기 테스트에서 스프링 컨텍스트 제거

--- src/test/java/co/fineants/api/domain/member/service/NicknameGeneratorTest.java
+++ src/test/java/co/fineants/api/domain/member/service/NicknameGeneratorTest.java
@@ -1,26 +1,26 @@
 package co.fineants.api.domain.member.service;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import co.fineants.AbstractContainerBaseTest;
+class NicknameGeneratorTest {
 
-class NicknameGeneratorTest extends AbstractContainerBaseTest {
-
-	@Autowired
 	private NicknameGenerator generator;
+
+	@BeforeEach
+	void setUp() {
+		generator = new NicknameGenerator("일개미", 7);
+	}
 
 	@DisplayName("회원의 랜덤 닉네임을 생성합니다")
 	@Test
 	void generate() {
 		// given
 		int expectedLength = 10;
-
 		// when
 		String nickname = generator.generate();
-
 		// then
 		Assertions.assertThat(nickname).hasSize(expectedLength);
 	}
