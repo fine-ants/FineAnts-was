be8412f22e03f296278314b0a96dc615845c1577
yonghwankim-dev <dragonbead95@naver.com>
2025-01-25 15:52:15 +0900

    feat: FakeAmazonS3Service 추가

--- src/main/java/co/fineants/api/infra/s3/service/AmazonS3Service.java
+++ src/main/java/co/fineants/api/infra/s3/service/AmazonS3Service.java
@@ -2,8 +2,10 @@ package co.fineants.api.infra.s3.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import co.fineants.api.global.errors.exception.BadRequestException;
+
 public interface AmazonS3Service {
-	String upload(MultipartFile multipartFile);
+	String upload(MultipartFile multipartFile) throws BadRequestException;
 
 	void deleteFile(String url);
 }
--- src/test/java/co/fineants/config/MemberServiceTestConfig.java
+++ src/test/java/co/fineants/config/MemberServiceTestConfig.java
@@ -5,11 +5,18 @@ import org.springframework.context.annotation.Bean;
 
 import co.fineants.api.infra.mail.EmailService;
 import co.fineants.api.infra.mail.FakeEmailService;
+import co.fineants.api.infra.s3.service.AmazonS3Service;
+import co.fineants.api.infra.s3.service.FakeAmazonS3Service;
 
 @TestConfiguration
 public class MemberServiceTestConfig {
 	@Bean
 	public EmailService emailService() {
 		return new FakeEmailService();
+	}
+
+	@Bean
+	public AmazonS3Service amazonS3Service() {
+		return new FakeAmazonS3Service();
 	}
 }
