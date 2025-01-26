7bf3b2aaffc8c74e4c2b58e979560c0c6a057051
yonghwankim-dev <dragonbead95@naver.com>
2025-01-26 14:46:16 +0900

    refactor: Fake 아마존 s3 서비스 클래스 제거

--- src/test/java/co/fineants/config/MemberServiceTestConfig.java
+++ src/test/java/co/fineants/config/MemberServiceTestConfig.java
@@ -1,27 +1,33 @@
 package co.fineants.config;
 
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 
 import co.fineants.api.infra.mail.EmailService;
 import co.fineants.api.infra.s3.service.AmazonS3Service;
-import lombok.RequiredArgsConstructor;
 
 @TestConfiguration
-@RequiredArgsConstructor
 public class MemberServiceTestConfig {
 
+	@Autowired
+	@Qualifier("defaultAmazonS3Service")
+	private AmazonS3Service defaultAmazonS3Service;
+
 	@Bean
-	@Primary
 	public EmailService mockEmailService() {
 		return Mockito.mock(EmailService.class);
 	}
 
 	@Bean
-	@Primary
 	public AmazonS3Service mockAmazonS3Service() {
 		return Mockito.mock(AmazonS3Service.class);
+	}
+
+	@Bean
+	public AmazonS3Service defaultAmazonS3Service() {
+		return defaultAmazonS3Service;
 	}
 }
