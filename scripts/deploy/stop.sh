694b0e9e15be574cd45c3122080ecf1d997bc501
yonghwankim-dev <dragonbead95@naver.com>
2025-02-18 17:13:42 +0900

    fix: 출력문 오타 수정

--- scripts/deploy/stop.sh
+++ scripts/deploy/stop.sh
@@ -6,13 +6,13 @@ if [ "$DEPLOYMENT_GROUP_NAME" == "release" ]; then
         cd /home/ec2-user/build/release || exit
         sudo docker-compose -f /home/ec2-user/build/release/docker-compose.release.yml down -v
     else
-        echo "docker-compose-release.yml 파일이 존재하지 않습니다."
+        echo "docker-compose.release.yml 파일이 존재하지 않습니다."
     fi
 elif [ "$DEPLOYMENT_GROUP_NAME" == "production" ]; then
     if [ -f /home/ec2-user/build/production/docker-compose.production.yml ]; then
         cd /home/ec2-user/build/production || exit
         sudo docker-compose -f /home/ec2-user/build/production/docker-compose.production.yml down -v
     else
-        echo "docker-compose-production.yml 파일이 존재하지 않습니다."
+        echo "docker-compose.production.yml 파일이 존재하지 않습니다."
     fi
 fi
