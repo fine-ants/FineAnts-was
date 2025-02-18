833dff5316f69b4f12dda8eb163f476624ab4899
yonghwankim-dev <dragonbead95@naver.com>
2025-02-18 16:46:11 +0900

    fix: 배포 스크립트에 디렉토리 생성 코드 추가

--- scripts/deploy/after-install.sh
+++ scripts/deploy/after-install.sh
@@ -2,11 +2,13 @@
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "release" ]
 then
+    mkdir -p /home/ec2-user/build/release  # 디렉토리 존재 여부 확인 후 생성
     cp -R /home/ec2-user/build/temp/* /home/ec2-user/build/release
 fi
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
 then
+    mkdir -p /home/ec2-user/build/production  # 디렉토리 존재 여부 확인 후 생성
     cp -R /home/ec2-user/build/temp/* /home/ec2-user/build/production
 fi
 # Clean Up
