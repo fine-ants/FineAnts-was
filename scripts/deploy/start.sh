71036ba82d0263466b1ba374faf500e95bf5dd6b
yonghwankim-dev <dragonbead95@naver.com>
2025-02-19 13:40:55 +0900

    docs: 배포 스크립트 변경

--- scripts/deploy/start.sh
+++ scripts/deploy/start.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+# scripts/start.sh
 
-# shellcheck disable=SC2034
+# 배포 환경을 PROFILE로 설정
 PROFILE="$DEPLOYMENT_GROUP_NAME"
+BASE_DIR="/home/ec2-user/build"
+DEPLOY_DIR="$BASE_DIR/$PROFILE"
+DOCKER_COMPOSE_FILE="$DEPLOY_DIR/docker-compose.$PROFILE.yml"
+CRON_SETUP_SCRIPT="$DEPLOY_DIR/scripts/setup_cron.sh"
 
-cd /home/ec2-user/build/"$PROFILE" || exit
+# 해당 배포 환경 디렉토리로 이동
+cd "$DEPLOY_DIR" || exit
 
+# Docker와 Docker Compose 실행 권한 설정
 sudo chmod 666 /var/run/docker.sock
 sudo chmod +x /usr/local/bin/docker-compose
-sudo docker-compose -f /home/ec2-user/build/"$PROFILE"/docker-compose."$PROFILE".yml pull
-sudo docker-compose -f /home/ec2-user/build/"$PROFILE"/docker-compose."$PROFILE".yml up -d
 
-# 실행하고 싶은 setup_cron.sh 파일 실행
-echo ">>> Setting up cron job" >> /home/ec2-user/build/"$PROFILE"/deploy.log
-sudo chmod +x /home/ec2-user/build/"$PROFILE"/scripts/setup_cron.sh
-sudo bash /home/ec2-user/build/"$PROFILE"/scripts/setup_cron.sh
+# docker-compose 실행
+sudo docker-compose -f "$DOCKER_COMPOSE_FILE" pull
+sudo docker-compose -f "$DOCKER_COMPOSE_FILE" up -d
+
+# cron job 설정 스크립트 실행
+echo ">>> Setting up cron job" >> "$DEPLOY_DIR/deploy.log"
+sudo chmod +x "$CRON_SETUP_SCRIPT"
+sudo bash "$CRON_SETUP_SCRIPT"
