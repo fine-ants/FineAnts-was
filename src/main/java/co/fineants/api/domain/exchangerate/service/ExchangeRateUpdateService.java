cd04d4404440d776662aff7ab70d9a4fb294c138
yonghwankim-dev <dragonbead95@naver.com>
2025-02-27 12:40:45 +0900

    style: 코드 정리

--- src/main/java/co/fineants/api/domain/exchangerate/service/ExchangeRateUpdateService.java
+++ src/main/java/co/fineants/api/domain/exchangerate/service/ExchangeRateUpdateService.java
@@ -11,12 +11,10 @@ import co.fineants.api.domain.exchangerate.domain.entity.ExchangeRate;
 import co.fineants.api.domain.exchangerate.repository.ExchangeRateRepository;
 import co.fineants.api.global.errors.errorcode.ExchangeRateErrorCode;
 import co.fineants.api.global.errors.exception.FineAntsException;
-import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-@Builder(toBuilder = true)
 public class ExchangeRateUpdateService {
 
 	private final ExchangeRateRepository exchangeRateRepository;
