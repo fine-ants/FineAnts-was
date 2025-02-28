6a6c3d5ced55c449af95d52e7c8d538f70723683
yonghwankim-dev <dragonbead95@naver.com>
2025-02-28 12:20:34 +0900

    test: Member 컨트롤러 수정

--- src/test/java/co/fineants/config/ControllerTestConfig.java
+++ src/test/java/co/fineants/config/ControllerTestConfig.java
@@ -4,9 +4,21 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import co.fineants.api.domain.fcm.service.FcmService;
+import co.fineants.api.domain.member.service.MemberNotificationPreferenceService;
+import co.fineants.api.domain.member.service.MemberNotificationService;
+import co.fineants.api.domain.member.service.MemberService;
 
 @TestConfiguration
 public class ControllerTestConfig {
 	@MockBean
 	private FcmService fcmService;
+
+	@MockBean
+	private MemberNotificationService notificationService;
+
+	@MockBean
+	private MemberNotificationPreferenceService preferenceService;
+
+	@MockBean
+	private MemberService memberService;
 }
