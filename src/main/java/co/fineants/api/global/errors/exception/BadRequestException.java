1b06aeffe82ef91dabfa7c7fdd9363d315c1e82b
yonghwankim-dev <dragonbead95@naver.com>
2025-03-04 16:37:51 +0900

    refactor: 생성자에 message 매개변수 추가

--- src/main/java/co/fineants/api/global/errors/exception/BadRequestException.java
+++ src/main/java/co/fineants/api/global/errors/exception/BadRequestException.java
@@ -1,5 +1,7 @@
 package co.fineants.api.global.errors.exception;
 
+import org.springframework.http.HttpStatus;
+
 import co.fineants.api.global.errors.errorcode.ErrorCode;
 
 public class BadRequestException extends FineAntsException {
@@ -8,7 +10,7 @@ public class BadRequestException extends FineAntsException {
 		super(errorCode);
 	}
 
-	public BadRequestException(ErrorCode errorCode, Throwable cause) {
-		super(errorCode, cause);
+	public BadRequestException(ErrorCode errorCode, String message, Throwable cause) {
+		super(errorCode, HttpStatus.BAD_REQUEST, message, cause);
 	}
 }
