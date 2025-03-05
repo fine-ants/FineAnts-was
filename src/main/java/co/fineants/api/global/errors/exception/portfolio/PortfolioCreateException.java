44da655de55ed580b6600ee364520603d7647392
yonghwankim-dev <dragonbead95@naver.com>
2025-03-05 12:43:45 +0900

    feat: 포트폴리오 생성 예외 클래스 추가

---
+++ src/main/java/co/fineants/api/global/errors/exception/portfolio/PortfolioCreateException.java
@@ -0,0 +1,7 @@
+package co.fineants.api.global.errors.exception.portfolio;
+
+public class PortfolioCreateException extends RuntimeException {
+	public PortfolioCreateException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
