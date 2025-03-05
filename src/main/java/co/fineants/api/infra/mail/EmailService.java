ec4c678a811c06d9e51ab15e84811b501c1784e7
yonghwankim-dev <dragonbead95@naver.com>
2025-03-05 14:41:48 +0900

    feat: 이메일 서비스 예외 처리

--- src/main/java/co/fineants/api/infra/mail/EmailService.java
+++ src/main/java/co/fineants/api/infra/mail/EmailService.java
@@ -1,5 +1,15 @@
 package co.fineants.api.infra.mail;
 
+import co.fineants.api.global.errors.exception.email.EmailSendException;
+
 public interface EmailService {
-	void sendEmail(String to, String subject, String body);
+	/**
+	 * 이메일을 전송한다
+	 *
+	 * @param to 수신자
+	 * @param subject 제목
+	 * @param body 내용
+	 * @throws EmailSendException 이메일을 전송하지 못하면 예외가 발생함
+	 */
+	void sendEmail(String to, String subject, String body) throws EmailSendException;
 }
--- src/main/java/co/fineants/api/infra/mail/JavaEmailService.java
+++ src/main/java/co/fineants/api/infra/mail/JavaEmailService.java
@@ -1,9 +1,11 @@
 package co.fineants.api.infra.mail;
 
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import co.fineants.api.global.errors.exception.email.EmailSendException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -12,11 +14,16 @@ public class JavaEmailService implements EmailService {
 	private final JavaMailSender mailSender;
 
 	@Override
-	public void sendEmail(String to, String subject, String body) {
+	public void sendEmail(String to, String subject, String body) throws EmailSendException {
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setTo(to);
 		message.setSubject(subject);
 		message.setText(body);
-		mailSender.send(message);
+		try {
+			mailSender.send(message);
+		} catch (MailException exception) {
+			String errorMessage = "can't send the email";
+			throw new EmailSendException(errorMessage, exception);
+		}
 	}
 }
