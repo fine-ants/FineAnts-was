5b1f3a7de35be2769e1260e5d126bc44f83f5605
yonghwankim-dev <dragonbead95@naver.com>
2025-03-06 15:00:08 +0900

    feat: Actuator 관련된 커스텀 설정 클래스 추가

---
+++ src/main/java/co/fineants/api/global/security/ajax/config/ActuatorProperties.java
@@ -0,0 +1,18 @@
+package co.fineants.api.global.security.ajax.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+
+@ConfigurationProperties(prefix = "actuator")
+public class ActuatorProperties {
+	private final String user;
+	private final String password;
+	private final String roleName;
+
+	@ConstructorBinding
+	public ActuatorProperties(String user, String password, String roleName) {
+		this.user = user;
+		this.password = password;
+		this.roleName = roleName;
+	}
+}
