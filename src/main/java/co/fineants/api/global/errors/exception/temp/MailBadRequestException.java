6dd4467f07b8ab9f8ad54a29cef5342acee9a36a
yonghwankim-dev <dragonbead95@naver.com>
2025-03-30 16:06:59 +0900

    feat: 메일 예외 클래스에 생성자 추가

--- src/main/java/co/fineants/api/global/errors/exception/temp/MailBadRequestException.java
+++ src/main/java/co/fineants/api/global/errors/exception/temp/MailBadRequestException.java
@@ -4,7 +4,11 @@ import co.fineants.api.global.errors.errorcode.CustomErrorCode;
 
 public class MailBadRequestException extends BadRequestException {
 
-	public MailBadRequestException(String value) {
-		super(value, CustomErrorCode.MAIL_BAD_REQUEST);
+	public MailBadRequestException(String mail) {
+		super(mail, CustomErrorCode.MAIL_BAD_REQUEST);
+	}
+
+	public MailBadRequestException(String mail, Throwable throwable) {
+		super(mail, CustomErrorCode.MAIL_BAD_REQUEST, throwable);
 	}
 }
