f5badc888c1a37ef91131005feeefad06d9b581c
yonghwankim-dev <dragonbead95@naver.com>
2025-04-07 13:18:15 +0900

    test: 환율 가져오기 테스트 추가

---
+++ src/test/java/co/fineants/api/domain/exchangerate/client/ExchangeRateWebClientTest.java
@@ -0,0 +1,54 @@
+package co.fineants.api.domain.exchangerate.client;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import co.fineants.api.domain.exchangerate.domain.dto.response.ExchangeRateFetchResponse;
+import co.fineants.api.domain.member.service.WebClientWrapper;
+import reactor.core.publisher.Mono;
+
+class ExchangeRateWebClientTest {
+
+	private ExchangeRateWebClient exchangeRateWebClient;
+
+	private WebClientWrapper webClient;
+
+	private String key;
+
+	@BeforeEach
+	void setUp() {
+		this.webClient = Mockito.mock(WebClientWrapper.class);
+		this.key = "test-key";
+		exchangeRateWebClient = new ExchangeRateWebClient(webClient, key);
+	}
+
+	@DisplayName("base가 주어지고 base 기준 환율들을 조회하면 환율을 조회한다")
+	@Test
+	void fetchRates() {
+		// given
+		String base = "KRW";
+		String uri = "https://exchange-rate-api1.p.rapidapi.com/latest?base=" + base.toUpperCase();
+		MultiValueMap<String, String> header = new LinkedMultiValueMap<>();
+		header.add("X-RapidAPI-Key", key);
+		header.add("X-RapidAPI-Host", "exchange-rate-api1.p.rapidapi.com");
+
+		BDDMockito.given(webClient.get(uri, header, ExchangeRateFetchResponse.class))
+			.willReturn(Mono.just(ExchangeRateFetchResponse.krw(Map.of("KRW", 1.0, "USD", 0.0006861))));
+		// when
+		Map<String, Double> actual = exchangeRateWebClient.fetchRates("KRW");
+		// then
+		Map<String, Double> expected = Map.of("KRW", 1.0, "USD", 0.0006861);
+		assertThat(actual)
+			.usingRecursiveComparison()
+			.isEqualTo(expected);
+	}
+}
