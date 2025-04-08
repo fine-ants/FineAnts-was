c9d024b48572c3c732b9d1f7d5d44b8ed819ad40
yonghwankim-dev <dragonbead95@naver.com>
2025-04-08 14:04:54 +0900

    style: 코드 정리

--- src/main/java/co/fineants/api/domain/exchangerate/client/WebClientWrapper.java
+++ src/main/java/co/fineants/api/domain/exchangerate/client/WebClientWrapper.java
@@ -1,14 +1,9 @@
 package co.fineants.api.domain.exchangerate.client;
 
-import java.util.function.Function;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import co.fineants.api.global.errors.exception.business.ExternalApiGetRequestException;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
@@ -29,19 +24,5 @@ public class WebClientWrapper {
 			.headers(header -> header.addAll(headerMap))
 			.retrieve()
 			.bodyToMono(responseType);
-	}
-	
-	private <T> Function<ClientResponse, Mono<T>> getClientResponseMonoFunction(Class<T> responseType) {
-		return clientResponse -> {
-			log.info("statusCode : {}", clientResponse.statusCode());
-			if (clientResponse.statusCode().is4xxClientError() || clientResponse.statusCode().is5xxServerError()) {
-				return clientResponse.bodyToMono(String.class).handle((body, sink) -> {
-					log.info("responseBody : {}", body);
-					HttpStatus httpStatus = HttpStatus.valueOf(clientResponse.statusCode().value());
-					sink.error(new ExternalApiGetRequestException(body, httpStatus));
-				});
-			}
-			return clientResponse.bodyToMono(responseType);
-		};
 	}
 }
