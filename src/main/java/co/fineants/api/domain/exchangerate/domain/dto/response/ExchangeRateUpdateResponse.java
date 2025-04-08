f6d97fcc39a36ca45d48210c7f020000b83a7007
yonghwankim-dev <dragonbead95@naver.com>
2025-04-08 16:53:09 +0900

    fix: jsonProperty 추가

--- src/main/java/co/fineants/api/domain/exchangerate/domain/dto/response/ExchangeRateUpdateResponse.java
+++ src/main/java/co/fineants/api/domain/exchangerate/domain/dto/response/ExchangeRateUpdateResponse.java
@@ -2,7 +2,10 @@ package co.fineants.api.domain.exchangerate.domain.dto.response;
 
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ExchangeRateUpdateResponse {
+	@JsonProperty
 	private final Map<String, Double> rates;
 
 	public ExchangeRateUpdateResponse(Map<String, Double> rates) {
