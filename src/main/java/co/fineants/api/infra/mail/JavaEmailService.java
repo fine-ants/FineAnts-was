0facb7011a189944f4e5a788526dca66b32a5be9
yonghwankim-dev <dragonbead95@naver.com>
2025-04-09 12:24:47 +0900

    feat: 관리자에게 환율 오류 메일 보내는 기능 구현

--- src/main/java/co/fineants/api/infra/mail/JavaEmailService.java
+++ src/main/java/co/fineants/api/infra/mail/JavaEmailService.java
@@ -1,15 +1,19 @@
 package co.fineants.api.infra.mail;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import lombok.RequiredArgsConstructor;
-
 @Service
-@RequiredArgsConstructor
 public class JavaEmailService implements EmailService {
 	private final JavaMailSender mailSender;
+	private final String adminEmail;
+
+	public JavaEmailService(JavaMailSender mailSender, @Value("${admin.email}") String adminEmail) {
+		this.mailSender = mailSender;
+		this.adminEmail = adminEmail;
+	}
 
 	@Override
 	public void sendEmail(String to, String subject, String body) {
@@ -17,6 +21,18 @@ public class JavaEmailService implements EmailService {
 		message.setTo(to);
 		message.setSubject(subject);
 		message.setText(body);
+		mailSender.send(message);
+	}
+
+	/**
+	 * 환율 API 서버로부터 환율 정보를 가져오지 못했을 때 발송하는 메일
+	 */
+	@Override
+	public void sendExchangeRateErrorEmail() {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(adminEmail);
+		message.setSubject("환율 API 서버 오류");
+		message.setText("환율 API 서버로부터 환율 정보를 가져오지 못했습니다. 서버 상태를 확인해주세요.");
 		mailSender.send(message);
 	}
 }
