9db8be2b62a5619947fbc8a4aeca38078f2565af
yonghwankim-dev <dragonbead95@naver.com>
2025-04-09 14:45:30 +0900

    feat: 에러 메일 전송시 예외 처리

--- src/main/java/co/fineants/api/infra/mail/JavaEmailService.java
+++ src/main/java/co/fineants/api/infra/mail/JavaEmailService.java
@@ -1,11 +1,15 @@
 package co.fineants.api.infra.mail;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class JavaEmailService implements EmailService {
 	private final JavaMailSender mailSender;
 	private final String adminEmail;
@@ -33,6 +37,10 @@ public class JavaEmailService implements EmailService {
 		message.setTo(adminEmail);
 		message.setSubject("환율 API 서버 오류");
 		message.setText("환율 API 서버로부터 환율 정보를 가져오지 못했습니다. 서버 상태를 확인해주세요.");
-		mailSender.send(message);
+		try {
+			mailSender.send(message);
+		} catch (MailException e) {
+			log.warn("환율 API 서버 오류 메일 발송 실패, adminEmail=" + adminEmail, e);
+		}
 	}
 }
