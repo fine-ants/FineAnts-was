4e56cac6a15934ecaa0b6e33426ee17e7535e72d
yonghwankim-dev <dragonbead95@naver.com>
2025-04-11 11:55:06 +0900

    feat: 환율 업데이트 예외 처리

--- src/main/java/co/fineants/api/domain/exchangerate/scheduler/ExchangeRateScheduler.java
+++ src/main/java/co/fineants/api/domain/exchangerate/scheduler/ExchangeRateScheduler.java
@@ -8,22 +8,29 @@ import org.springframework.transaction.annotation.Transactional;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 
 import co.fineants.api.domain.exchangerate.service.ExchangeRateUpdateService;
+import co.fineants.api.global.errors.exception.business.BaseExchangeRateNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @RequiredArgsConstructor
 @Profile(value = {"production", "release"})
+@Slf4j
 public class ExchangeRateScheduler {
 
 	private final ExchangeRateUpdateService service;
 
 	/**
-	 * 매일 0시 0분에 환율을 업데이트합니다.
+	 * 매일 00시 0분 0초에 한번씩 환율을 업데이트합니다.
 	 */
 	@SchedulerLock(name = "exchangeRateScheduler", lockAtLeastFor = "1m", lockAtMostFor = "1m")
 	@Scheduled(cron = "0 0 * * * *")
 	@Transactional
 	public void updateExchangeRates() {
-		service.updateExchangeRates();
+		try {
+			service.updateExchangeRates();
+		} catch (BaseExchangeRateNotFoundException e) {
+			log.error("환율 업데이트 실패", e);
+		}
 	}
 }
