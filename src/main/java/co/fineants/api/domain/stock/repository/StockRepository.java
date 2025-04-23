85d567f54640cf44e740a9cc6321237d89f55f80
yonghwankim-dev <dragonbead95@naver.com>
2025-04-23 15:44:24 +0900

    fix: 종목 조회 쿼리 오타 수정

--- src/main/java/co/fineants/api/domain/stock/repository/StockRepository.java
+++ src/main/java/co/fineants/api/domain/stock/repository/StockRepository.java
@@ -19,7 +19,7 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
 	@Query("select s from Stock s where s.tickerSymbol = :tickerSymbol")
 	Optional<Stock> findByTickerSymbolIncludingDeleted(@Param("tickerSymbol") String tickerSymbol);
 
-	@Query("select distinct s from Stock s join fetch s.stockDividends sd where s.tickerSymbol in (:tickerSymbols)")
+	@Query("select distinct s from Stock s left join fetch s.stockDividends sd where s.tickerSymbol in (:tickerSymbols)")
 	List<Stock> findAllWithDividends(@Param("tickerSymbols") List<String> tickerSymbols);
 
 	@Query("select s from Stock s where s.stockCode like %:keyword% or s.tickerSymbol like %:keyword% or s.companyName like %:keyword% or s.companyNameEng like %:keyword%")
