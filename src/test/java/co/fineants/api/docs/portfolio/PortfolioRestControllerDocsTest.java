c5914e224c2ea21db1857005836184b83c1b6744
yonghwankim-dev <dragonbead95@naver.com>
2025-05-02 15:05:37 +0900

    fix: 오타 수정

--- src/test/java/co/fineants/api/docs/portfolio/PortfolioRestControllerDocsTest.java
+++ src/test/java/co/fineants/api/docs/portfolio/PortfolioRestControllerDocsTest.java
@@ -251,7 +251,7 @@ class PortfolioRestControllerDocsTest extends RestDocsSupport {
 					preprocessResponse(prettyPrint()),
 					queryParameters(
 						parameterWithName("page").description("페이지 번호(0부터 시작, 기본값: 0)").optional(),
-						parameterWithName("size").description("페이지 크기(한 페이지에 포함된 항목 개수, 기본값: 10)").optional(),
+						parameterWithName("size").description("페이지 크기(한 페이지에 포함된 항목 개수, 기본값: 50)").optional(),
 						parameterWithName("direction").description("생성일자 기준 정렬 방향(ASC 또는 DESC, 기본값: DESC)").optional()
 					),
 					responseFields(
