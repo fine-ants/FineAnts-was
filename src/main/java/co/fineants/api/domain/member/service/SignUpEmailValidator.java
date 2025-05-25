601026bfc1630e642ed2892113242cacf281fb49
yonghwankim-dev <dragonbead95@naver.com>
2025-05-25 16:27:38 +0900

    refactor: change signiture - String value to Member

--- src/main/java/co/fineants/api/domain/member/service/SignUpEmailValidator.java
+++ src/main/java/co/fineants/api/domain/member/service/SignUpEmailValidator.java
@@ -4,6 +4,7 @@ import static co.fineants.api.domain.member.service.MemberService.*;
 
 import org.apache.logging.log4j.util.Strings;
 
+import co.fineants.api.domain.member.domain.entity.Member;
 import co.fineants.api.domain.member.domain.entity.MemberProfile;
 import co.fineants.api.domain.member.repository.MemberRepository;
 import co.fineants.api.global.errors.exception.business.EmailDuplicateException;
@@ -18,7 +19,8 @@ public class SignUpEmailValidator implements SignUpValidator {
 	}
 
 	@Override
-	public void validate(String email) throws EmailInvalidInputException, EmailDuplicateException {
+	public void validate(Member member) throws EmailInvalidInputException, EmailDuplicateException {
+		String email = member.getEmail();
 		if (Strings.isBlank(email)) {
 			throw new EmailInvalidInputException(email);
 		}
--- src/main/java/co/fineants/api/domain/member/service/SignUpValidator.java
+++ src/main/java/co/fineants/api/domain/member/service/SignUpValidator.java
@@ -1,5 +1,7 @@
 package co.fineants.api.domain.member.service;
 
+import co.fineants.api.domain.member.domain.entity.Member;
+
 public interface SignUpValidator {
-	void validate(String value);
+	void validate(Member member);
 }
