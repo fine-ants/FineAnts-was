3a129816ba84ffa6c70729f06c0a98900701d0d4
yonghwankim-dev <dragonbead95@naver.com>
2025-05-26 16:30:51 +0900

    refactor: change interface - MemberValidator to Validator<T>

--- src/main/java/co/fineants/api/domain/member/domain/rule/MemberValidator.java
+++
@@ -1,7 +0,0 @@
-package co.fineants.api.domain.member.domain.rule;
-
-import co.fineants.api.domain.member.domain.entity.Member;
-
-public interface MemberValidator {
-	void validate(Member member);
-}
