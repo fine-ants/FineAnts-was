b1ac7678188bb1d79c4ac499af1b066e64a858c5
yonghwankim-dev <dragonbead95@naver.com>
2025-05-26 15:30:50 +0900

    feat: add spring config class - MemberAssociationRegistarationConfig

---
+++ src/main/java/co/fineants/api/domain/member/service/MemberAssociationRegistrationService.java
@@ -0,0 +1,7 @@
+package co.fineants.api.domain.member.service;
+
+import co.fineants.api.domain.member.domain.entity.Member;
+
+public interface MemberAssociationRegistrationService {
+	void registerAll(Member member);
+}
