b89f4aed2223a49e10512cc426bdf4db9b57c695
yonghwankim-dev <dragonbead95@naver.com>
2025-05-26 13:39:30 +0900

    add spring config class - RuleConfig

---
+++ src/main/java/co/fineants/api/domain/member/domain/rule/SignUpValidator.java
@@ -0,0 +1,21 @@
+package co.fineants.api.domain.member.domain.rule;
+
+import java.util.Arrays;
+import java.util.List;
+
+import co.fineants.api.domain.member.domain.entity.Member;
+
+public class SignUpValidator implements MemberValidator {
+	private final List<ValidationRule> rules;
+
+	public SignUpValidator(ValidationRule... rules) {
+		this.rules = Arrays.asList(rules);
+	}
+
+	@Override
+	public void validate(Member member) {
+		for (ValidationRule rule : rules) {
+			rule.validate(member);
+		}
+	}
+}
