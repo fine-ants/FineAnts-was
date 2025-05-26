4b87df41b05f0a274e6883eb7e5b2e5272667c8e
yonghwankim-dev <dragonbead95@naver.com>
2025-05-26 14:04:37 +0900

    add class - MemberRoleFactory

---
+++ src/main/java/co/fineants/api/domain/member/service/MemberRoleFactory.java
@@ -0,0 +1,22 @@
+package co.fineants.api.domain.member.service;
+
+import co.fineants.api.domain.member.domain.entity.Member;
+import co.fineants.api.domain.member.domain.entity.MemberRole;
+import co.fineants.api.domain.member.domain.entity.Role;
+import co.fineants.api.domain.member.repository.RoleRepository;
+import co.fineants.api.global.errors.exception.business.RoleNotFoundException;
+
+public class MemberRoleFactory {
+
+	private final RoleRepository roleRepository;
+
+	public MemberRoleFactory(RoleRepository roleRepository) {
+		this.roleRepository = roleRepository;
+	}
+
+	public MemberRole userMemberRole(Member member) {
+		Role userRole = roleRepository.findRoleByRoleName("ROLE_USER")
+			.orElseThrow(() -> new RoleNotFoundException("ROLE_USER"));
+		return new MemberRole(member, userRole);
+	}
+}
