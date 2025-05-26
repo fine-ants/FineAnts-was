4d395f00662903766794969f370617f0ccc5847e
yonghwankim-dev <dragonbead95@naver.com>
2025-05-26 15:12:43 +0900

    add interface - MemberAssociationRegister

---
+++ src/main/java/co/fineants/api/domain/member/domain/MemberAssociationRegistrar.java
@@ -0,0 +1,7 @@
+package co.fineants.api.domain.member.domain;
+
+import co.fineants.api.domain.member.domain.entity.Member;
+
+public interface MemberAssociationRegistrar {
+	void register(Member member);
+}
