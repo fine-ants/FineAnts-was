858b910fc9c9c4d29d1a44490077645a763ca1d3
yonghwankim-dev <dragonbead95@naver.com>
2025-05-26 15:44:50 +0900

    fix: fix typo

--- src/main/java/co/fineants/api/domain/member/domain/DefaultNotificationPreferenceSettingRegistrar.java
+++ src/main/java/co/fineants/api/domain/member/domain/DefaultNotificationPreferenceSettingRegistrar.java
@@ -3,11 +3,11 @@ package co.fineants.api.domain.member.domain;
 import co.fineants.api.domain.member.domain.entity.Member;
 import co.fineants.api.domain.member.service.MemberNotificationPreferenceService;
 
-public class DefaultNotificationPreferenceSettingRegister implements MemberAssociationRegistrar {
+public class DefaultNotificationPreferenceSettingRegistrar implements MemberAssociationRegistrar {
 
 	private final MemberNotificationPreferenceService service;
 
-	public DefaultNotificationPreferenceSettingRegister(MemberNotificationPreferenceService service) {
+	public DefaultNotificationPreferenceSettingRegistrar(MemberNotificationPreferenceService service) {
 		this.service = service;
 	}
 
