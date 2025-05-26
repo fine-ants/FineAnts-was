6a3f2ba1760462f17b4b3d926edc70c6ea663338
yonghwankim-dev <dragonbead95@naver.com>
2025-05-26 14:57:36 +0900

    add spring config class - MemberRoleConfig

---
+++ src/main/java/co/fineants/api/domain/member/config/MemberRoleConfig.java
@@ -0,0 +1,15 @@
+package co.fineants.api.domain.member.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import co.fineants.api.domain.member.service.MemberRoleFactory;
+import co.fineants.api.domain.member.service.RoleService;
+
+@Configuration
+public class MemberRoleConfig {
+	@Bean
+	public MemberRoleFactory memberRoleFactory(RoleService roleService) {
+		return new MemberRoleFactory(roleService);
+	}
+}
--- src/main/java/co/fineants/api/domain/member/service/MemberRoleFactory.java
+++ src/main/java/co/fineants/api/domain/member/service/MemberRoleFactory.java
@@ -3,21 +3,18 @@ package co.fineants.api.domain.member.service;
 import co.fineants.api.domain.member.domain.entity.Member;
 import co.fineants.api.domain.member.domain.entity.MemberRole;
 import co.fineants.api.domain.member.domain.entity.Role;
-import co.fineants.api.domain.member.repository.RoleRepository;
-import co.fineants.api.global.errors.exception.business.RoleNotFoundException;
 
 public class MemberRoleFactory {
 
-	private final RoleRepository roleRepository;
+	private final RoleService roleService;
 
-	public MemberRoleFactory(RoleRepository roleRepository) {
-		this.roleRepository = roleRepository;
+	public MemberRoleFactory(RoleService roleService) {
+		this.roleService = roleService;
 	}
 
 	public MemberRole userMemberRole(Member member) {
 		String roleName = "ROLE_USER";
-		Role userRole = roleRepository.findRoleByRoleName(roleName)
-			.orElseThrow(() -> new RoleNotFoundException(roleName));
+		Role userRole = roleService.findRole(roleName);
 		return new MemberRole(member, userRole);
 	}
 }
