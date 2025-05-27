5976bd44a28221e6b11fd162131748232cebdc9d
yonghwankim-dev <dragonbead95@naver.com>
2025-05-27 16:01:41 +0900

    refactor: add field - localProvider field

--- src/main/java/co/fineants/api/domain/member/domain/rule/EmailDuplicationRule.java
+++ src/main/java/co/fineants/api/domain/member/domain/rule/EmailDuplicationRule.java
@@ -1,7 +1,5 @@
 package co.fineants.api.domain.member.domain.rule;
 
-import static co.fineants.api.domain.member.service.MemberService.*;
-
 import co.fineants.api.domain.member.domain.entity.Member;
 import co.fineants.api.domain.member.repository.MemberRepository;
 import co.fineants.api.global.errors.exception.business.EmailDuplicateException;
@@ -9,14 +7,16 @@ import co.fineants.api.global.errors.exception.business.EmailDuplicateException;
 public class EmailDuplicationRule implements ValidationRule {
 
 	private final MemberRepository memberRepository;
+	private final String localProvider;
 
-	public EmailDuplicationRule(MemberRepository memberRepository) {
+	public EmailDuplicationRule(MemberRepository memberRepository, String localProvider) {
 		this.memberRepository = memberRepository;
+		this.localProvider = localProvider;
 	}
 
 	@Override
 	public void validate(String email) {
-		if (memberRepository.findMemberByEmailAndProvider(email, LOCAL_PROVIDER).isPresent()) {
+		if (memberRepository.findMemberByEmailAndProvider(email, localProvider).isPresent()) {
 			throw new EmailDuplicateException(email);
 		}
 	}
