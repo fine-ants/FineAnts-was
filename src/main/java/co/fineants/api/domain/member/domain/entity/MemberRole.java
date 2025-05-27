4579ba1a59912d011572044730a153916d8cca57
yonghwankim-dev <dragonbead95@naver.com>
2025-05-27 15:45:45 +0900

    fix: change constructor accessModifier - MemberRole default-package to private

--- src/main/java/co/fineants/api/domain/member/domain/entity/MemberRole.java
+++ src/main/java/co/fineants/api/domain/member/domain/entity/MemberRole.java
@@ -32,7 +32,7 @@ public class MemberRole {
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Role role;
 
-	MemberRole(Member member, Role role) {
+	private MemberRole(Member member, Role role) {
 		this.member = member;
 		this.role = role;
 	}
