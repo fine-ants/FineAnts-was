a3a84fa58ba28e8ff76c1658a6d512b150f8e58b
yonghwankim-dev <dragonbead95@naver.com>
2025-05-28 16:10:53 +0900

    feat: add @RequiredArgsConstructor

--- src/main/java/co/fineants/api/domain/member/service/SignupVerificationService.java
+++ src/main/java/co/fineants/api/domain/member/service/SignupVerificationService.java
@@ -2,31 +2,21 @@ package co.fineants.api.domain.member.service;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import co.fineants.api.domain.member.domain.factory.MimeMessageFactory;
 import co.fineants.api.infra.mail.EmailService;
 import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class SignupVerificationService {
 
 	private final VerifyCodeGenerator generator;
 	private final VerifyCodeManagementService codeService;
-	private final MimeMessageFactory mimeMessageFactory;
+	private final MimeMessageFactory verifyCodeMimeMessageFactory;
 	private final EmailService emailService;
-
-	public SignupVerificationService(
-		VerifyCodeGenerator generator,
-		VerifyCodeManagementService codeService,
-		@Qualifier("verifyCodeMimeMessageFactory") MimeMessageFactory mimeMessageFactory,
-		EmailService emailService) {
-		this.generator = generator;
-		this.codeService = codeService;
-		this.mimeMessageFactory = mimeMessageFactory;
-		this.emailService = emailService;
-	}
 
 	public void sendSignupVerification(String email) {
 		// 검증 코드 생성
@@ -35,7 +25,7 @@ public class SignupVerificationService {
 		codeService.saveVerifyCode(email, verifyCode);
 		// 이메일 메시지 생성
 		Map<String, Object> variables = Map.of("verifyCode", verifyCode);
-		MimeMessage message = mimeMessageFactory.create(email, variables);
+		MimeMessage message = verifyCodeMimeMessageFactory.create(email, variables);
 		// 검증 코드 이메일 전송
 		emailService.sendEmail(message);
 	}
