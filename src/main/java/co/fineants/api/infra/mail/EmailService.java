7d06e0c5e09c4146dc8a409b71553510206dbdd4
yonghwankim-dev <dragonbead95@naver.com>
2025-05-28 16:22:30 +0900

    fix: remove test

--- src/main/java/co/fineants/api/infra/mail/EmailService.java
+++ src/main/java/co/fineants/api/infra/mail/EmailService.java
@@ -1,12 +1,8 @@
 package co.fineants.api.infra.mail;
 
-import java.util.Map;
-
 import jakarta.mail.internet.MimeMessage;
 
 public interface EmailService {
-
-	void sendEmail(String to, String subject, String templateName, Map<String, Object> variables);
 
 	void sendEmail(MimeMessage message);
 }
