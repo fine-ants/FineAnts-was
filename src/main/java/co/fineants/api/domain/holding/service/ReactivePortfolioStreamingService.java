c25b9ea69d1f980f805b52517141a02e4a0d6ad1
yonghwankim-dev <dragonbead95@naver.com>
2025-06-02 13:40:54 +0900

    feat: add field - interval, maxCount

--- src/main/java/co/fineants/api/domain/holding/service/ReactivePortfolioStreamingService.java
+++ src/main/java/co/fineants/api/domain/holding/service/ReactivePortfolioStreamingService.java
@@ -9,15 +9,20 @@ import reactor.core.scheduler.Schedulers;
 public class ReactivePortfolioStreamingService implements PortfolioStreamingService {
 
 	private final PortfolioHoldingService portfolioHoldingService;
+	private final Duration interval;
+	private final int maxCount;
 
-	public ReactivePortfolioStreamingService(PortfolioHoldingService portfolioHoldingService) {
+	public ReactivePortfolioStreamingService(PortfolioHoldingService portfolioHoldingService, int second,
+		int maxCount) {
 		this.portfolioHoldingService = portfolioHoldingService;
+		this.interval = Duration.ofSeconds(second);
+		this.maxCount = maxCount;
 	}
 
 	@Override
 	public Flux<PortfolioHoldingsRealTimeResponse> streamPortfolioReturns(Long portfolioId) {
-		return Flux.interval(Duration.ofSeconds(5))
-			.take(6)
+		return Flux.interval(interval)
+			.take(maxCount)
 			.publishOn(Schedulers.boundedElastic())
 			.map(i -> portfolioHoldingService.readMyPortfolioStocksInRealTime(portfolioId));
 	}
