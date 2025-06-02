2a7f1fdd984da563bcb9da8f72a97fa53f4be9ff
yonghwankim-dev <dragonbead95@naver.com>
2025-06-02 14:14:52 +0900

    rename: rename interface, class - PortfolioStreamingService to PortfolioStreamer

--- src/main/java/co/fineants/api/domain/holding/service/ReactivePortfolioStreamer.java
+++ src/main/java/co/fineants/api/domain/holding/service/ReactivePortfolioStreamer.java
@@ -6,13 +6,13 @@ import co.fineants.api.domain.holding.domain.dto.response.PortfolioHoldingsRealT
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 
-public class ReactivePortfolioStreamingService implements PortfolioStreamingService {
+public class ReactivePortfolioStreamer implements PortfolioStreamer {
 
 	private final PortfolioHoldingService portfolioHoldingService;
 	private final Duration interval;
 	private final int maxCount;
 
-	public ReactivePortfolioStreamingService(PortfolioHoldingService portfolioHoldingService, int second,
+	public ReactivePortfolioStreamer(PortfolioHoldingService portfolioHoldingService, int second,
 		int maxCount) {
 		this.portfolioHoldingService = portfolioHoldingService;
 		this.interval = Duration.ofSeconds(second);
@@ -20,7 +20,7 @@ public class ReactivePortfolioStreamingService implements PortfolioStreamingServ
 	}
 
 	@Override
-	public Flux<PortfolioHoldingsRealTimeResponse> streamPortfolioReturns(Long portfolioId) {
+	public Flux<PortfolioHoldingsRealTimeResponse> streamReturns(Long portfolioId) {
 		return Flux.interval(interval)
 			.take(maxCount)
 			.publishOn(Schedulers.boundedElastic())
