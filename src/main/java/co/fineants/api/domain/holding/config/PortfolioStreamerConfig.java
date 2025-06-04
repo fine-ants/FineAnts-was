7738e077e297a2d5b3737618c9916d8ffe6effe7
yonghwankim-dev <dragonbead95@naver.com>
2025-06-04 15:14:29 +0900

    feat: add filter - stockMarketCheker

--- src/main/java/co/fineants/api/domain/holding/config/PortfolioStreamerConfig.java
+++ src/main/java/co/fineants/api/domain/holding/config/PortfolioStreamerConfig.java
@@ -5,13 +5,16 @@ import org.springframework.context.annotation.Configuration;
 
 import co.fineants.api.domain.holding.service.FluxIntervalPortfolioStreamer;
 import co.fineants.api.domain.holding.service.PortfolioHoldingService;
+import co.fineants.api.domain.holding.service.StockMarketChecker;
+import co.fineants.api.global.common.time.LocalDateTimeService;
 
 @Configuration
 public class PortfolioStreamerConfig {
 	@Bean
-	public FluxIntervalPortfolioStreamer fluxIntervalPortfolioStreamer(PortfolioHoldingService service) {
+	public FluxIntervalPortfolioStreamer fluxIntervalPortfolioStreamer(PortfolioHoldingService service,
+		StockMarketChecker stockMarketChecker, LocalDateTimeService localDateTimeService) {
 		int second = 5;
 		int maxCount = 6;
-		return new FluxIntervalPortfolioStreamer(service, second, maxCount);
+		return new FluxIntervalPortfolioStreamer(service, stockMarketChecker, localDateTimeService, second, maxCount);
 	}
 }
--- src/test/java/co/fineants/api/domain/holding/service/FluxIntervalPortfolioStreamerTest.java
+++ src/test/java/co/fineants/api/domain/holding/service/FluxIntervalPortfolioStreamerTest.java
@@ -1,6 +1,9 @@
 package co.fineants.api.domain.holding.service;
 
+import static org.mockito.BDDMockito.*;
+
 import java.time.Duration;
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -8,18 +11,28 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import co.fineants.api.domain.holding.domain.dto.response.PortfolioHoldingsRealTimeResponse;
+import co.fineants.api.global.common.time.LocalDateTimeService;
+import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 class FluxIntervalPortfolioStreamerTest {
 
 	private PortfolioStreamer service;
+	private StockMarketChecker stockMarketChecker;
+	private LocalDateTimeService localDateTimeService;
 
 	@BeforeEach
 	void setUp() {
 		PortfolioHoldingService holdingService = Mockito.mock(PortfolioHoldingService.class);
+		LocalDateTime dateTime = LocalDateTime.of(2025, 6, 4, 9, 0);
+		stockMarketChecker = new StockMarketChecker();
+		localDateTimeService = Mockito.mock(LocalDateTimeService.class);
+		given(localDateTimeService.getLocalDateTimeWithNow())
+			.willReturn(dateTime); // 기본적으로 장 시간을 반환하도록 설정
 		int second = 5;
 		int maxCount = 6;
-		service = new FluxIntervalPortfolioStreamer(holdingService, second, maxCount);
+		service = new FluxIntervalPortfolioStreamer(holdingService, stockMarketChecker, localDateTimeService, second,
+			maxCount);
 
 		PortfolioHoldingsRealTimeResponse response = Mockito.mock(PortfolioHoldingsRealTimeResponse.class);
 		Mockito.when(holdingService.readMyPortfolioStocksInRealTime(Mockito.anyLong()))
@@ -35,6 +48,23 @@ class FluxIntervalPortfolioStreamerTest {
 		StepVerifier.withVirtualTime(() -> service.streamReturns(portfolioId))
 			.thenAwait(Duration.ofSeconds(30))
 			.expectNextCount(6)
+			.verifyComplete();
+	}
+
+	@DisplayName("장시간이 끝난후에는 빈 스트림을 반환한다.")
+	@Test
+	void givenPortfolioId_whenMarketIsClosed_thenReturnEmptyStream() {
+		// given
+		long portfolioId = 1L;
+		LocalDateTime closedTime = LocalDateTime.of(2025, 6, 4, 15, 30);
+		given(localDateTimeService.getLocalDateTimeWithNow())
+			.willReturn(closedTime);
+		// when
+		Flux<PortfolioHoldingsRealTimeResponse> flux = service.streamReturns(portfolioId);
+		// then
+		StepVerifier.create(flux)
+			.expectSubscription()
+			.expectNextCount(0)
 			.verifyComplete();
 	}
 }
