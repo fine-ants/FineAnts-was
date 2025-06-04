5cd908d479c5383df58bcd9cd82a273035272fb5
yonghwankim-dev <dragonbead95@naver.com>
2025-06-04 15:14:29 +0900

    feat: add filter - stockMarketCheker

--- src/main/java/co/fineants/api/domain/holding/service/FluxIntervalPortfolioStreamer.java
+++ src/main/java/co/fineants/api/domain/holding/service/FluxIntervalPortfolioStreamer.java
@@ -1,8 +1,10 @@
 package co.fineants.api.domain.holding.service;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 
 import co.fineants.api.domain.holding.domain.dto.response.PortfolioHoldingsRealTimeResponse;
+import co.fineants.api.global.common.time.LocalDateTimeService;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
@@ -11,20 +13,26 @@ import reactor.core.scheduler.Schedulers;
 public class FluxIntervalPortfolioStreamer implements PortfolioStreamer {
 
 	private final PortfolioHoldingService portfolioHoldingService;
+	private final StockMarketChecker stockMarketChecker;
+	private final LocalDateTimeService localDateTimeService;
 	private final Duration interval;
 	private final int maxCount;
 
-	public FluxIntervalPortfolioStreamer(PortfolioHoldingService portfolioHoldingService, int second,
-		int maxCount) {
+	public FluxIntervalPortfolioStreamer(PortfolioHoldingService portfolioHoldingService,
+		StockMarketChecker stockMarketChecker, LocalDateTimeService localDateTimeService, int second, int maxCount) {
 		this.portfolioHoldingService = portfolioHoldingService;
+		this.stockMarketChecker = stockMarketChecker;
+		this.localDateTimeService = localDateTimeService;
 		this.interval = Duration.ofSeconds(second);
 		this.maxCount = maxCount;
 	}
 
 	@Override
 	public Flux<PortfolioHoldingsRealTimeResponse> streamReturns(Long portfolioId) {
+		LocalDateTime nowDateTime = localDateTimeService.getLocalDateTimeWithNow();
 		return Flux.interval(interval)
 			.take(maxCount)
+			.filter(i -> stockMarketChecker.isMarketOpen(nowDateTime))
 			.publishOn(Schedulers.boundedElastic())
 			.map(i -> portfolioHoldingService.readMyPortfolioStocksInRealTime(portfolioId));
 	}
