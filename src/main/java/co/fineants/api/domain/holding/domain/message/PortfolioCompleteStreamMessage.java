7517ab2d22a86d8b923a57074fbba720fb55e3c4
yonghwankim-dev <dragonbead95@naver.com>
2025-06-05 12:21:53 +0900

    feat: implement ClosedMarketPortfolioStreamer.streamMessages()

---
+++ src/main/java/co/fineants/api/domain/holding/domain/message/PortfolioCompleteStreamMessage.java
@@ -0,0 +1,17 @@
+package co.fineants.api.domain.holding.domain.message;
+
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+public class PortfolioCompleteStreamMessage implements PortfolioStreamMessage {
+	private final String message;
+
+	public PortfolioCompleteStreamMessage() {
+		this.message = "sse complete";
+	}
+
+	@Override
+	public String toString() {
+		return String.format("포트폴리오 완료 메시지 - %s", message);
+	}
+}
---
+++ src/test/java/co/fineants/api/domain/holding/service/ClosedMarketPortfolioStreamerTest.java
@@ -0,0 +1,36 @@
+package co.fineants.api.domain.holding.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import co.fineants.api.domain.holding.domain.message.PortfolioCompleteStreamMessage;
+import co.fineants.api.domain.holding.domain.message.PortfolioStreamMessage;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+class ClosedMarketPortfolioStreamerTest {
+
+	private PortfolioStreamer portfolioStreamer;
+
+	@BeforeEach
+	void setUp() {
+		StockMarketChecker stockMarketChecker = Mockito.mock(StockMarketChecker.class);
+		portfolioStreamer = new ClosedMarketPortfolioStreamer(stockMarketChecker);
+	}
+
+	@DisplayName("장시간이 아닌 경우에는 완료 메시지를 스트리밍한다")
+	@Test
+	void whenMarketIsClose_thenReturnCompleteMessage() {
+		// given
+		Long portfolioId = 1L;
+		// when
+		Flux<PortfolioStreamMessage> flux = portfolioStreamer.streamMessages(portfolioId);
+		// then
+		PortfolioCompleteStreamMessage expected = new PortfolioCompleteStreamMessage();
+		StepVerifier.create(flux)
+			.expectNext(expected)
+			.verifyComplete();
+	}
+}
