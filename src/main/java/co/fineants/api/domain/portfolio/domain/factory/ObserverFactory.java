9d39027ae33b6ac7a0488d1cbbc63e7b11b5d115
yonghwankim-dev <dragonbead95@naver.com>
2025-06-05 13:33:25 +0900

    refactor: remove class

--- src/main/java/co/fineants/api/domain/portfolio/domain/factory/ObserverFactory.java
+++
@@ -1,9 +0,0 @@
-package co.fineants.api.domain.portfolio.domain.factory;
-
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import io.reactivex.rxjava3.core.Observer;
-
-public interface ObserverFactory<R> {
-	Observer<R> create(SseEmitter emitter);
-}
