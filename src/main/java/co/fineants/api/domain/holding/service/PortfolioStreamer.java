649ba53327f5c7747e4b241d3ecff511a2f9141a
yonghwankim-dev <dragonbead95@naver.com>
2025-06-05 13:44:25 +0900

    refactor: remove class

--- src/main/java/co/fineants/api/domain/holding/service/PortfolioStreamer.java
+++ src/main/java/co/fineants/api/domain/holding/service/PortfolioStreamer.java
@@ -2,12 +2,10 @@ package co.fineants.api.domain.holding.service;
 
 import java.time.LocalDateTime;
 
-import co.fineants.api.domain.holding.domain.dto.response.PortfolioHoldingsRealTimeResponse;
 import co.fineants.api.domain.holding.domain.message.PortfolioStreamMessage;
 import reactor.core.publisher.Flux;
 
 public interface PortfolioStreamer {
-	Flux<PortfolioHoldingsRealTimeResponse> streamReturns(Long portfolioId);
 
 	Flux<PortfolioStreamMessage> streamMessages(Long portfolioId);
 
