5b6d7e53efadc377ce0bc9eae1aa2b6d01644bdb
yonghwankim-dev <dragonbead95@naver.com>
2025-06-05 14:07:26 +0900

    refactor: remove interface - PortfolioReturnsService

--- src/main/java/co/fineants/api/domain/holding/service/PortfolioReturnsService.java
+++
@@ -1,7 +0,0 @@
-package co.fineants.api.domain.holding.service;
-
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-public interface PortfolioReturnsService {
-	void streamReturns(Long portfolioId, SseEmitter emitter);
-}
