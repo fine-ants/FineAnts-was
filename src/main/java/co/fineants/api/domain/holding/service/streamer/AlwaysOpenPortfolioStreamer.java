2dfc613800c681e4b9e75d6daf029048ffb96c94
yonghwankim-dev <dragonbead95@naver.com>
2025-06-09 16:26:49 +0900

    feat: add PortfolioStreamer - AlwaysOpenPortfolioStreamer

---
+++ src/main/java/co/fineants/api/domain/holding/service/streamer/AlwaysOpenPortfolioStreamer.java
@@ -0,0 +1,35 @@
+package co.fineants.api.domain.holding.service.streamer;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import co.fineants.api.domain.holding.domain.message.StreamMessage;
+import co.fineants.api.domain.holding.service.PortfolioHoldingService;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class AlwaysOpenPortfolioStreamer implements PortfolioStreamer {
+	private final PortfolioHoldingService portfolioHoldingService;
+	private final Duration interval;
+	private final long maxCount;
+
+	public AlwaysOpenPortfolioStreamer(PortfolioHoldingService portfolioHoldingService, Duration interval,
+		long maxCount) {
+		this.portfolioHoldingService = portfolioHoldingService;
+		this.interval = interval;
+		this.maxCount = maxCount;
+	}
+
+	@Override
+	public Flux<StreamMessage> streamMessages(Long portfolioId) {
+		return Flux.interval(interval)
+			.take(maxCount)
+			.publishOn(Schedulers.boundedElastic())
+			.map(i -> portfolioHoldingService.getPortfolioReturns(portfolioId));
+	}
+
+	@Override
+	public boolean supports(LocalDateTime time) {
+		return true;
+	}
+}
