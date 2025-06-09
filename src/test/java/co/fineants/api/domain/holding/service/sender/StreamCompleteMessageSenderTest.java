a08d7d3bbd609b34b9a5b61fb78d8f235110440c
yonghwankim-dev <dragonbead95@naver.com>
2025-06-09 15:55:16 +0900

    test: fix test

--- src/test/java/co/fineants/api/domain/holding/service/sender/StreamCompleteMessageSenderTest.java
+++ src/test/java/co/fineants/api/domain/holding/service/sender/StreamCompleteMessageSenderTest.java
@@ -10,33 +10,24 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import co.fineants.api.domain.holding.domain.factory.SseEventBuilderFactory;
-import co.fineants.api.domain.holding.domain.message.PortfolioCompleteStreamMessage;
-import co.fineants.api.domain.holding.domain.message.StreamMessage;
-
 class StreamCompleteMessageSenderTest {
 
 	private StreamSseMessageSender sender;
 	private SseEmitter emitter;
-	private SseEventBuilderFactory sseEventBuilderFactory;
 
 	@BeforeEach
 	void setUp() {
 		emitter = Mockito.mock(SseEmitter.class);
-		sseEventBuilderFactory = Mockito.mock(SseEventBuilderFactory.class);
-		sender = new StreamCompleteMessageSender(emitter, sseEventBuilderFactory);
+		sender = new StreamCompleteMessageSender(emitter);
 	}
 
 	@DisplayName("StreamMessage를 SseEmitter로 전송한다.")
 	@Test
 	void givenStreamMessage_whenAcceptMessage_thenSendEmitter() throws IOException {
 		// given
-		StreamMessage message = new PortfolioCompleteStreamMessage();
 		SseEmitter.SseEventBuilder builder = Mockito.mock(SseEmitter.SseEventBuilder.class);
-		given(sseEventBuilderFactory.create(message))
-			.willReturn(builder);
 		// when
-		sender.accept(message);
+		sender.accept(builder);
 		// then
 		verify(emitter).send(builder);
 		verify(emitter).complete();
