e008da128b9b710c30cff3d3be7309c0372e8938
yonghwankim-dev <dragonbead95@naver.com>
2025-06-09 15:32:44 +0900

    test: add test - accept

---
+++ src/test/java/co/fineants/api/domain/holding/service/sender/StreamContinuesMessageSenderTest.java
@@ -0,0 +1,47 @@
+package co.fineants.api.domain.holding.service.sender;
+
+import static org.mockito.BDDMockito.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import co.fineants.api.domain.holding.domain.factory.SseEventBuilderFactory;
+import co.fineants.api.domain.holding.domain.message.PortfolioReturnsStreamMessage;
+import co.fineants.api.domain.holding.domain.message.StreamMessage;
+
+class StreamContinuesMessageSenderTest {
+
+	private StreamSseMessageSender sender;
+	private SseEmitter emitter;
+	private SseEventBuilderFactory sseEventBuilderFactory;
+
+	@BeforeEach
+	void setUp() {
+		emitter = Mockito.mock(SseEmitter.class);
+		sseEventBuilderFactory = Mockito.mock(SseEventBuilderFactory.class);
+		sender = new StreamContinuesMessageSender(emitter, 3000L);
+	}
+
+	@DisplayName("StreamMessage가 주어지고 메시지를 접수하면 SseEmitter에 SseEventBuilder를 전송한다.")
+	@Test
+	void givenStreamMessage_whenAcceptMessage_thenSendSseEventBuilder() throws IOException {
+		// given
+		StreamMessage message = Mockito.mock(PortfolioReturnsStreamMessage.class);
+		SseEmitter.SseEventBuilder builder = Mockito.mock(SseEmitter.SseEventBuilder.class);
+		given(sseEventBuilderFactory.create(message))
+			.willReturn(builder);
+		// when
+		sender.accept(message);
+		// then
+		BDDMockito.verify(emitter).send(ArgumentMatchers.any(SseEmitter.SseEventBuilder.class));
+		BDDMockito.verify(emitter, never()).complete();
+		BDDMockito.verify(emitter, never()).completeWithError(any());
+	}
+}
