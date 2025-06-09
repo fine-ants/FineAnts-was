50acc48a1bc64d3ddb513f63697b3903111e4460
yonghwankim-dev <dragonbead95@naver.com>
2025-06-09 15:59:21 +0900

    feat: add Spring Bean - PortfolioSseEventBuilderFactory

--- src/main/java/co/fineants/api/domain/holding/config/SseEmitterFactoryConfig.java
+++ src/main/java/co/fineants/api/domain/holding/config/SseEmitterFactoryConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import co.fineants.api.domain.holding.domain.factory.PortfolioSseEmitterFactory;
+import co.fineants.api.domain.holding.domain.factory.PortfolioSseEventBuilderFactory;
 
 @Configuration
 public class SseEmitterFactoryConfig {
@@ -12,5 +13,11 @@ public class SseEmitterFactoryConfig {
 	@Bean
 	public PortfolioSseEmitterFactory portfolioSseEmitterFactory(@Value("${portfolio.timeout:30000}") long timeout) {
 		return new PortfolioSseEmitterFactory(timeout);
+	}
+
+	@Bean
+	public PortfolioSseEventBuilderFactory portfolioSseEventBuilderFactory(
+		@Value("${sse.portfolio.reconnectTimeMillis:3000}") long reconnectTimeMillis) {
+		return new PortfolioSseEventBuilderFactory(reconnectTimeMillis);
 	}
 }
