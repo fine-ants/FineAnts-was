9fff8c078267d4295f06f695de4353b2155b9357
yonghwankim-dev <dragonbead95@naver.com>
2025-06-10 14:11:28 +0900

    test: add test - HolidayMarketStatusCheckerRule.isOpen

---
+++ src/test/java/co/fineants/api/domain/holding/service/market_status_checker/HolidayMarketStatusCheckerRuleTest.java
@@ -0,0 +1,33 @@
+package co.fineants.api.domain.holding.service.market_status_checker;
+
+import java.time.LocalDateTime;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import co.fineants.AbstractContainerBaseTest;
+import co.fineants.api.domain.holiday.domain.entity.Holiday;
+import co.fineants.api.domain.holiday.repository.HolidayRepository;
+
+class HolidayMarketStatusCheckerRuleTest extends AbstractContainerBaseTest {
+
+	@Autowired
+	private HolidayRepository repository;
+
+	@DisplayName("dateTime이 휴장일인 경우에는 false를 반환한다")
+	@Test
+	void isOpen_shouldReturnFalse_whenDateTimeIsHoliday() {
+		// given
+		MarketStatusCheckerRule rule = new HolidayMarketStatusCheckerRule(repository);
+		LocalDateTime dateTime = LocalDateTime.of(2025, 7, 6, 9, 0);
+		Holiday holiday = Holiday.close(dateTime.toLocalDate());
+		repository.save(holiday);
+		// when
+		boolean isOpen = rule.isOpen(dateTime);
+		// then
+		Assertions.assertThat(isOpen).isFalse();
+	}
+
+}
