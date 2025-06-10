ac46ae1edc1a55fae4b31ccec9c83159056965af
yonghwankim-dev <dragonbead95@naver.com>
2025-06-10 14:19:41 +0900

    refactor: introduce field - rule

--- src/test/java/co/fineants/api/domain/holding/service/market_status_checker/HolidayMarketStatusCheckerRuleTest.java
+++ src/test/java/co/fineants/api/domain/holding/service/market_status_checker/HolidayMarketStatusCheckerRuleTest.java
@@ -1,8 +1,10 @@
 package co.fineants.api.domain.holding.service.market_status_checker;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.time.LocalDateTime;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,30 +17,34 @@ class HolidayMarketStatusCheckerRuleTest extends AbstractContainerBaseTest {
 
 	@Autowired
 	private HolidayRepository repository;
+	private LocalDateTime dateTime;
+	private MarketStatusCheckerRule rule;
+
+	@BeforeEach
+	void setUp() {
+		dateTime = LocalDateTime.of(2025, 6, 6, 9, 0);
+		rule = new HolidayMarketStatusCheckerRule(repository);
+	}
 
 	@DisplayName("dateTime이 공휴일인 경우에는 false를 반환한다")
 	@Test
 	void isOpen_shouldReturnFalse_whenDateTimeIsHoliday() {
 		// given
-		MarketStatusCheckerRule rule = new HolidayMarketStatusCheckerRule(repository);
-		LocalDateTime dateTime = LocalDateTime.of(2025, 7, 6, 9, 0);
 		Holiday holiday = Holiday.close(dateTime.toLocalDate());
 		repository.save(holiday);
 		// when
 		boolean isOpen = rule.isOpen(dateTime);
 		// then
-		Assertions.assertThat(isOpen).isFalse();
+		assertThat(isOpen).isFalse();
 	}
 
 	@DisplayName("dateTime이 공휴일이 아닌 경우에는 true를 반환한다")
 	@Test
 	void isOpen_shouldReturnTrue_whenDateTimeIsNotHoliday() {
 		// given
-		MarketStatusCheckerRule rule = new HolidayMarketStatusCheckerRule(repository);
-		LocalDateTime dateTime = LocalDateTime.of(2025, 7, 6, 9, 0);
 		// when
 		boolean isOpen = rule.isOpen(dateTime);
 		// then
-		Assertions.assertThat(isOpen).isTrue();
+		assertThat(isOpen).isTrue();
 	}
 }
