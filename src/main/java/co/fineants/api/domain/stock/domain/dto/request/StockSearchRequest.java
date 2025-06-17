c9fab71da8e40129bc7562d9545be68f3cb66fdc
yonghwankim-dev <dragonbead95@naver.com>
2025-06-17 13:05:50 +0900

    add json annotation

--- src/main/java/co/fineants/api/domain/stock/domain/dto/request/StockSearchRequest.java
+++ src/main/java/co/fineants/api/domain/stock/domain/dto/request/StockSearchRequest.java
@@ -1,12 +1,16 @@
 package co.fineants.api.domain.stock.domain.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 
 @Getter
 public class StockSearchRequest {
 	private String searchTerm;
 
-	public StockSearchRequest(String searchTerm) {
+	@JsonCreator
+	public StockSearchRequest(@JsonProperty("searchTerm") String searchTerm) {
 		this.searchTerm = searchTerm;
 	}
 }
