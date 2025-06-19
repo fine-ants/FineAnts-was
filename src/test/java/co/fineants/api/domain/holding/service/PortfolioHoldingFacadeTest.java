66202eef1bae94a04bfd89f4831bfa10a7e0b277
yonghwankim-dev <dragonbead95@naver.com>
2025-06-19 15:37:35 +0900

    test: add test - with PurchaseHistory add case

--- src/test/java/co/fineants/api/domain/holding/service/PortfolioHoldingFacadeTest.java
+++ src/test/java/co/fineants/api/domain/holding/service/PortfolioHoldingFacadeTest.java
@@ -17,6 +17,7 @@ import co.fineants.api.domain.member.repository.MemberRepository;
 import co.fineants.api.domain.portfolio.domain.entity.Portfolio;
 import co.fineants.api.domain.portfolio.repository.PortfolioRepository;
 import co.fineants.api.domain.purchasehistory.domain.dto.request.PurchaseHistoryCreateRequest;
+import co.fineants.api.domain.purchasehistory.repository.PurchaseHistoryRepository;
 import co.fineants.api.domain.stock.domain.entity.Stock;
 import co.fineants.api.domain.stock.repository.StockRepository;
 import co.fineants.api.global.errors.exception.business.ForbiddenException;
@@ -34,6 +35,9 @@ class PortfolioHoldingFacadeTest extends AbstractContainerBaseTest {
 
 	@Autowired
 	private StockRepository stockRepository;
+
+	@Autowired
+	private PurchaseHistoryRepository purchaseHistoryRepository;
 
 	@DisplayName("사용자는 다른 사용자의 포트폴리오를 대상으로 포트폴리오 종목을 추가할 수 없다")
 	@Test
@@ -61,6 +65,7 @@ class PortfolioHoldingFacadeTest extends AbstractContainerBaseTest {
 			.isInstanceOf(ForbiddenException.class);
 	}
 
+	@DisplayName("포트폴리오 종목 생성 시 매입 이력 생성 요청이 null인 경우, 포트폴리오 종목만 저장한다")
 	@Test
 	void createPortfolioHolding_whenOnlyPortfolioHolding_thenSavePortfolioHolding() {
 		// given
@@ -75,5 +80,31 @@ class PortfolioHoldingFacadeTest extends AbstractContainerBaseTest {
 
 		// then
 		Assertions.assertThat(portfolioHolding).isNotNull();
+	}
+
+	@DisplayName("포트폴리오 종목 생성 시 매입 이력 생성 요청이 null이 아닌 경우, 포트폴리오 종목과 매입 이력을 저장한다")
+	@Test
+	void createPortfolioHolding_whenPurchaseHistoryCreateRequestIsNotNull_thenSavePortfolioHoldingAndPurchaseHistory() {
+		// given
+		Member member = memberRepository.save(createMember());
+		setAuthentication(member);
+		Portfolio portfolio = portfolioRepository.save(createPortfolio(member));
+		Stock samsung = stockRepository.save(createSamsungStock());
+
+		PurchaseHistoryCreateRequest purchaseHistoryCreateRequest = PurchaseHistoryCreateRequest.create(
+			LocalDateTime.now(),
+			Count.from(3),
+			Money.won(50_000),
+			"memo"
+		);
+		PortfolioHoldingCreateRequest request = PortfolioHoldingCreateRequest.create(samsung.getTickerSymbol(),
+			purchaseHistoryCreateRequest);
+		// when
+		PortfolioHolding portfolioHolding = portfolioHoldingFacade.createPortfolioHolding(request, portfolio.getId());
+
+		// then
+		Assertions.assertThat(portfolioHolding).isNotNull();
+		Assertions.assertThat(purchaseHistoryRepository.findAllByPortfolioHoldingId(portfolioHolding.getId()))
+			.hasSize(1);
 	}
 }
