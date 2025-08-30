68f7e3dc1d72109be5035bc420cd2b829a313d91
yonghwankim-dev <dragonbead95@naver.com>
2025-08-30 16:06:11 +0900

    update test
    검증 방식을 포함에서 라인 비교로 변경

--- src/test/java/co/fineants/api/infra/s3/service/DividendCsvFormatterTest.java
+++ src/test/java/co/fineants/api/infra/s3/service/DividendCsvFormatterTest.java
@@ -1,8 +1,19 @@
 package co.fineants.api.infra.s3.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.time.LocalDate;
+
 import org.assertj.core.api.Assertions;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import co.fineants.api.domain.common.money.Money;
+import co.fineants.api.domain.dividend.domain.entity.StockDividend;
+import co.fineants.api.domain.stock.domain.entity.Market;
+import co.fineants.api.domain.stock.domain.entity.Stock;
 
 class DividendCsvFormatterTest {
 
@@ -29,5 +40,33 @@ class DividendCsvFormatterTest {
 		Assertions.assertThat(result).isNotNull();
 		Assertions.assertThat(parseCsvHeader(result))
 			.contains("id,dividend,recordDate,paymentDate,stockCode");
+	}
+
+	@Test
+	void format_whenDataIsOne() throws IOException {
+		StockDividend stockDividend = createSamsungStockDividend();
+		String result = formatter.format(stockDividend);
+
+		Assertions.assertThat(result).isNotNull();
+		BufferedReader lead = new BufferedReader(new StringReader(result));
+		BufferedReader gold = new BufferedReader(new StringReader("id,dividend,recordDate,paymentDate,stockCode"));
+		String line;
+		while ((line = gold.readLine()) != null) {
+			String readLine = lead.readLine();
+			Assertions.assertThat(readLine).isEqualTo(line);
+		}
+		Assertions.assertThat(lead.readLine()).isNull();
+	}
+
+	@NotNull
+	private static StockDividend createSamsungStockDividend() {
+		Long id = 1L;
+		Money dividend = Money.won(361);
+		LocalDate recordDate = LocalDate.of(2023, 3, 31);
+		LocalDate exDividendDate = LocalDate.of(2023, 3, 30);
+		LocalDate paymentDate = LocalDate.of(2024, 5, 17);
+		Stock stock = Stock.of("005930", "삼성전자보통주", "SamsungElectronics", "KR7005930003", "전기전자", Market.KOSPI);
+		return StockDividend.create(id, dividend, recordDate, exDividendDate, paymentDate,
+			stock);
 	}
 }
