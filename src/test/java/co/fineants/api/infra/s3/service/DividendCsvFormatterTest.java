818954c16aaf6229a71bb8939b32bf695db6df6a
yonghwankim-dev <dragonbead95@naver.com>
2025-08-30 15:45:15 +0900

    rename test name

--- src/test/java/co/fineants/api/infra/s3/service/DividendCsvFormatterTest.java
+++ src/test/java/co/fineants/api/infra/s3/service/DividendCsvFormatterTest.java
@@ -1,24 +1,29 @@
 package co.fineants.api.infra.s3.service;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DividendCsvFormatterTest {
+
+	private DividendCsvFormatter formatter;
 
 	private String parseCsvHeader(String result) {
 		return result.split("\n")[0];
 	}
 
+	@BeforeEach
+	void setUp() {
+		formatter = new DividendCsvFormatter();
+	}
+
 	@Test
 	void canCreated() {
-		DividendCsvFormatter formatter = new DividendCsvFormatter();
 		Assertions.assertThat(formatter).isNotNull();
 	}
 
 	@Test
-	void format() {
-		DividendCsvFormatter formatter = new DividendCsvFormatter();
-
+	void format_whenDataIsEmpty() {
 		String result = formatter.format();
 
 		Assertions.assertThat(result).isNotNull();
