72af14516d55aca8ed45b81c8dd80f7ec568fe0c
yonghwankim-dev <dragonbead95@naver.com>
2025-09-01 13:48:21 +0900

    extract method - compareContent

--- src/test/java/co/fineants/api/infra/s3/service/FileContentComparator.java
+++ src/test/java/co/fineants/api/infra/s3/service/FileContentComparator.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.io.StringReader;
 
 import org.assertj.core.api.Assertions;
@@ -11,30 +12,27 @@ import org.assertj.core.api.Assertions;
 public class FileContentComparator {
 
 	public void compare(String content, String goldFilePath) {
-		try {
-			BufferedReader lead = new BufferedReader(new StringReader(content));
-			BufferedReader gold = new BufferedReader(new FileReader(goldFilePath));
-
-			String line;
-			while ((line = gold.readLine()) != null) {
-				Assertions.assertThat(lead.readLine()).isEqualTo(line);
-			}
-			Assertions.assertThat(lead.readLine()).isNull();
+		try (StringReader reader = new StringReader(content)) {
+			compareContent(reader, goldFilePath);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	public void compare(File file, String goldFilePath) {
-		try {
-			BufferedReader lead = new BufferedReader(new FileReader(file));
-			BufferedReader gold = new BufferedReader(new FileReader(goldFilePath));
+	private void compareContent(Reader reader, String goldFilePath) throws IOException {
+		BufferedReader lead = new BufferedReader(reader);
+		BufferedReader gold = new BufferedReader(new FileReader(goldFilePath));
 
-			String line;
-			while ((line = gold.readLine()) != null) {
-				Assertions.assertThat(lead.readLine()).isEqualTo(line);
-			}
-			Assertions.assertThat(lead.readLine()).isNull();
+		String line;
+		while ((line = gold.readLine()) != null) {
+			Assertions.assertThat(lead.readLine()).isEqualTo(line);
+		}
+		Assertions.assertThat(lead.readLine()).isNull();
+	}
+
+	public void compare(File file, String goldFilePath) {
+		try (FileReader reader = new FileReader(file)) {
+			compareContent(reader, goldFilePath);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
