bedb5bb58151b4cbf2d9cefcaa78b4f0b996d4fc
yonghwankim-dev <dragonbead95@naver.com>
2025-09-01 13:08:22 +0900

    add field - bucketName, dividendPath, amazonS3

--- src/main/java/co/fineants/api/infra/s3/service/AmazonS3FetchDividendService.java
+++ src/main/java/co/fineants/api/infra/s3/service/AmazonS3FetchDividendService.java
@@ -2,9 +2,28 @@ package co.fineants.api.infra.s3.service;
 
 import java.io.File;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import com.amazonaws.services.s3.AmazonS3;
+
 public class AmazonS3FetchDividendService implements FetchDividendService {
+
+	private final String bucketName;
+	private final String dividendPath;
+	private final AmazonS3 amazonS3;
+
+	public AmazonS3FetchDividendService(
+		@Value("${aws.s3.bucket}") String bucketName,
+		@Value("${aws.s3.dividend-csv-path}") String dividendPath,
+		AmazonS3 amazonS3) {
+		this.bucketName = bucketName;
+		this.dividendPath = dividendPath;
+		this.amazonS3 = amazonS3;
+	}
+
 	@Override
 	public File fetchDividend() {
+
 		return new File("dividends.csv");
 	}
 }
--- src/test/java/co/fineants/api/infra/s3/service/AmazonS3FetchDividendServiceTest.java
+++ src/test/java/co/fineants/api/infra/s3/service/AmazonS3FetchDividendServiceTest.java
@@ -4,17 +4,26 @@ import java.io.File;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.amazonaws.services.s3.AmazonS3;
 
 class AmazonS3FetchDividendServiceTest {
 	@Test
 	void canCreated() {
-		FetchDividendService service = new AmazonS3FetchDividendService();
+		String bucketName = "fineants2024";
+		String dividendPath = "local/dividend/dividends.csv";
+		AmazonS3 amazonS3 = Mockito.mock(AmazonS3.class);
+		FetchDividendService service = new AmazonS3FetchDividendService(bucketName, dividendPath, amazonS3);
 		Assertions.assertThat(service).isNotNull();
 	}
 
 	@Test
 	void fetchDividend() {
-		FetchDividendService service = new AmazonS3FetchDividendService();
+		String bucketName = "fineants2024";
+		String dividendPath = "local/dividend/dividends.csv";
+		AmazonS3 amazonS3 = Mockito.mock(AmazonS3.class);
+		FetchDividendService service = new AmazonS3FetchDividendService(bucketName, dividendPath, amazonS3);
 
 		File file = service.fetchDividend();
 
