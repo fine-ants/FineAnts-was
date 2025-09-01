5a80394f4381fc0350f3b55bf929193842439c11
yonghwankim-dev <dragonbead95@naver.com>
2025-09-01 10:41:54 +0900

    introduce field - amazonS3, Uplaoder

--- src/test/java/co/fineants/api/infra/s3/service/AmazonBucketS3FileUploaderTest.java
+++ src/test/java/co/fineants/api/infra/s3/service/AmazonBucketS3FileUploaderTest.java
@@ -1,6 +1,7 @@
 package co.fineants.api.infra.s3.service;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -10,24 +11,28 @@ import joptsimple.internal.Strings;
 
 class AmazonBucketS3FileUploaderTest {
 
+	private FileUploader fileUploader;
+	private AmazonS3 amazonS3;
+
+	@BeforeEach
+	void setUp() {
+		String bucketName = "fineants2024";
+		amazonS3 = Mockito.mock(AmazonS3.class);
+		fileUploader = new AmazonBucketS3FileUploader(bucketName, amazonS3);
+	}
+
 	@Test
 	void canCreated() {
-		String bucketName = "fineants2024";
-		AmazonS3 amazonS3 = Mockito.mock(AmazonS3.class);
-		FileUploader fileUploader = new AmazonBucketS3FileUploader(bucketName, amazonS3);
-
 		Assertions.assertThat(fileUploader).isNotNull();
 	}
 
 	@Test
 	void upload_whenFileContentIsEmpty_thenUploadEmptyFile() {
-		String bucketName = "fineants2024";
-		AmazonS3 amazonS3 = Mockito.mock(AmazonS3.class);
-		FileUploader fileUploader = new AmazonBucketS3FileUploader(bucketName, amazonS3);
 		String fileContent = Strings.EMPTY;
 		String filePath = "local/dividend/dividends.csv";
 
 		Assertions.assertThatCode(() -> fileUploader.upload(fileContent, filePath))
 			.doesNotThrowAnyException();
+		Mockito.verify(amazonS3, Mockito.times(1)).putObject(Mockito.any());
 	}
 }
