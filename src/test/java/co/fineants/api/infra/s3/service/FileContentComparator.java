2484d0301ca76a325d2c6bd922a5105a23ff4ee2
yonghwankim-dev <dragonbead95@naver.com>
2025-09-01 13:41:28 +0900

    add method - compare(File,String)

--- src/test/java/co/fineants/api/infra/s3/service/FileContentComparator.java
+++ src/test/java/co/fineants/api/infra/s3/service/FileContentComparator.java
@@ -1,6 +1,7 @@
 package co.fineants.api.infra.s3.service;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -34,6 +35,21 @@ public class FileContentComparator {
 	public void compare(InputStream inputStream, String goldFilePath) {
 		try (InputStreamReader reader = new InputStreamReader(inputStream)) {
 			compareContent(reader, goldFilePath);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void compare(File file, String goldFilePath) {
+		try {
+			BufferedReader lead = new BufferedReader(new FileReader(file));
+			BufferedReader gold = new BufferedReader(new FileReader(goldFilePath));
+
+			String line;
+			while ((line = gold.readLine()) != null) {
+				Assertions.assertThat(lead.readLine()).isEqualTo(line);
+			}
+			Assertions.assertThat(lead.readLine()).isNull();
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
