135d2b5f053ee1c3ee8bffb888f6c8509747a9b0
yonghwankim-dev <dragonbead95@naver.com>
2025-09-02 14:48:35 +0900

    delete unused method - fetchStocks

--- src/main/java/co/fineants/api/infra/s3/service/AmazonS3StockService.java
+++ src/main/java/co/fineants/api/infra/s3/service/AmazonS3StockService.java
@@ -3,14 +3,10 @@ package co.fineants.api.infra.s3.service;
 import static co.fineants.api.domain.stock.service.StockCsvReader.*;
 import static java.nio.charset.StandardCharsets.*;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.util.Strings;
@@ -18,16 +14,12 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
-import com.amazonaws.services.s3.model.S3Object;
 
 import co.fineants.api.domain.stock.domain.entity.Stock;
-import co.fineants.api.domain.stock.parser.StockParser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -35,46 +27,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class AmazonS3StockService {
-
-	public static final String CSV_SEPARATOR_REGEX = "\\$";
-
+	
 	private final AmazonS3 amazonS3;
 	@Value("${aws.s3.bucket}")
 	private String bucketName;
 	@Value("${aws.s3.stock-path}")
 	private String stockPath;
-
-	private final StockParser stockParser;
-
-	// todo: extract interface
-	public List<Stock> fetchStocks() {
-		return getS3Object()
-			.map(this::parseStocks)
-			.orElseGet(Collections::emptyList);
-	}
-
-	private List<Stock> parseStocks(S3Object s3Object) {
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(s3Object.getObjectContent(), UTF_8))) {
-			return br.lines()
-				.skip(1) // skip title
-				.map(line -> line.split(CSV_SEPARATOR_REGEX))
-				.map(stockParser::parse)
-				.distinct()
-				.toList();
-		} catch (Exception e) {
-			log.error(e.getMessage());
-			return Collections.emptyList();
-		}
-	}
-
-	private Optional<S3Object> getS3Object() {
-		try {
-			return Optional.ofNullable(amazonS3.getObject(new GetObjectRequest(bucketName, stockPath)));
-		} catch (AmazonServiceException e) {
-			log.error(e.getMessage());
-			return Optional.empty();
-		}
-	}
 
 	public void writeStocks(List<Stock> stocks) {
 		String title = csvTitle();
