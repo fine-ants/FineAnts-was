e8689cb25d09d604be1dec584ef06a367fb9eed4
yonghwankim-dev <dragonbead95@naver.com>
2025-09-02 13:56:07 +0900

    implement fetchStocks

--- src/main/java/co/fineants/api/infra/s3/service/AmazonS3FetchStockService.java
+++ src/main/java/co/fineants/api/infra/s3/service/AmazonS3FetchStockService.java
@@ -1,14 +1,41 @@
 package co.fineants.api.infra.s3.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 import co.fineants.api.domain.stock.domain.entity.Stock;
+import co.fineants.api.domain.stock.parser.StockParser;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class AmazonS3FetchStockService implements FetchStockService {
+
+	private static final String CSV_SEPARATOR_REGEX = "\\$";
+
+	private final RemoteFileFetcher fetcher;
+	private final StockParser stockParser;
+
+	public AmazonS3FetchStockService(RemoteFileFetcher fetcher, StockParser stockParser) {
+		this.fetcher = fetcher;
+		this.stockParser = stockParser;
+	}
 
 	@Override
 	public List<Stock> fetchStocks() {
-		return new ArrayList<>();
+		try (BufferedReader reader = new BufferedReader(
+			new InputStreamReader(fetcher.read("local/stock/stocks.csv")))) {
+			return reader.lines()
+				.skip(1) // skip header line
+				.map(line -> line.split(CSV_SEPARATOR_REGEX))
+				.map(stockParser::parse)
+				.distinct()
+				.toList();
+		} catch (IOException e) {
+			log.error("Error reading stocks file", e);
+			return new ArrayList<>();
+		}
 	}
 }
