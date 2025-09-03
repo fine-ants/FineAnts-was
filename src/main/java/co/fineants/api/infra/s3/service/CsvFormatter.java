6af94bac728f91a21f7dc9ab5173a2017779a7c8
yonghwankim-dev <dragonbead95@naver.com>
2025-09-03 12:03:59 +0900

    Create CsvFormatter
    도메인별 formatter가 아닌 제네릭 하게 하기 위해서 구현함

---
+++ src/main/java/co/fineants/api/infra/s3/service/CsvFormatter.java
@@ -0,0 +1,35 @@
+package co.fineants.api.infra.s3.service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.util.Strings;
+import org.jetbrains.annotations.NotNull;
+
+import co.fineants.api.domain.stock.domain.entity.Stock;
+
+public class CsvFormatter<T> {
+
+	private final String delimiter;
+	private final String[] headers;
+
+	public CsvFormatter(String delimiter, String[] headers) {
+		this.delimiter = delimiter;
+		this.headers = headers;
+	}
+
+	public String format(T... items) {
+		String title = String.join(delimiter, headers);
+		String lines = createLines(Arrays.asList(items));
+		return String.join(Strings.LINE_SEPARATOR, title, lines).trim();
+	}
+
+	@NotNull
+	private String createLines(List<T> items) {
+		return items.stream()
+			.map(item -> (Stock)item)
+			.map(Stock::toCsvLineString)
+			.collect(Collectors.joining(Strings.LINE_SEPARATOR));
+	}
+}
