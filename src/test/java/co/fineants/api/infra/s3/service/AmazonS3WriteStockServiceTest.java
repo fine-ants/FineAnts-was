1709f73ef2e4b80ff8486b2b2ef8855e8511f915
yonghwankim-dev <dragonbead95@naver.com>
2025-09-03 11:42:30 +0900

    add constructor in AmazonS3WriteStockService

--- src/test/java/co/fineants/api/infra/s3/service/AmazonS3WriteStockServiceTest.java
+++ src/test/java/co/fineants/api/infra/s3/service/AmazonS3WriteStockServiceTest.java
@@ -4,19 +4,26 @@ import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class AmazonS3WriteStockServiceTest {
 
 	@Test
 	void canCreated() {
-		WriteStockService service = new AmazonS3WriteStockService();
+		RemoteFileUploader uploader = Mockito.mock(RemoteFileUploader.class);
+		String filePath = "local/stock/stocks.csv";
+		StockCsvFormatter formatter = new StockCsvFormatter();
+		WriteStockService service = new AmazonS3WriteStockService(uploader, filePath, formatter);
 
 		Assertions.assertThat(service).isNotNull();
 	}
 
 	@Test
 	void writeStocks() {
-		WriteStockService service = new AmazonS3WriteStockService();
+		RemoteFileUploader uploader = Mockito.mock(RemoteFileUploader.class);
+		String filePath = "local/stock/stocks.csv";
+		StockCsvFormatter formatter = new StockCsvFormatter();
+		WriteStockService service = new AmazonS3WriteStockService(uploader, filePath, formatter);
 
 		Assertions.assertThatCode(() -> service.writeStocks(List.of())).doesNotThrowAnyException();
 	}
