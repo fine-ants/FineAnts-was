6496b79c3bc711aa96345d4019de7119fad67dd9
yonghwankim-dev <dragonbead95@naver.com>
2025-09-03 11:10:51 +0900

    fix stock sector

--- src/test/java/co/fineants/api/infra/s3/service/StockCsvFormatterTest.java
+++ src/test/java/co/fineants/api/infra/s3/service/StockCsvFormatterTest.java
@@ -3,6 +3,9 @@ package co.fineants.api.infra.s3.service;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import co.fineants.TestDataFactory;
+import co.fineants.api.domain.stock.domain.entity.Stock;
+
 class StockCsvFormatterTest {
 
 	@Test
@@ -19,5 +22,15 @@ class StockCsvFormatterTest {
 		String content = formatter.format();
 
 		Assertions.assertThat(content).isEqualTo("stockCode$tickerSymbol$companyName$companyNameEng$sector$market");
+	}
+
+	@Test
+	void format_whenStockIsOne() {
+		StockCsvFormatter formatter = new StockCsvFormatter();
+		Stock stock = TestDataFactory.createSamsungStock();
+
+		String content = formatter.format(stock);
+
+		new FileContentComparator().compare(content, "src/test/resources/gold_stocks.csv");
 	}
 }
