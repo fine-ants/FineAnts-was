3d2368f1646ded15fe587686be0ccd22c2e360ed
yonghwankim-dev <dragonbead95@naver.com>
2025-09-03 16:40:31 +0900

    create ProfileImageFile class

--- src/main/java/co/fineants/api/infra/s3/service/AmazonS3RemoteFileUploader.java
+++ src/main/java/co/fineants/api/infra/s3/service/AmazonS3RemoteFileUploader.java
@@ -3,8 +3,6 @@ package co.fineants.api.infra.s3.service;
 import static java.nio.charset.StandardCharsets.*;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -20,18 +18,12 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 import co.fineants.api.domain.holding.domain.factory.UuidGenerator;
-import co.fineants.api.global.errors.exception.business.ImageEmptyInvalidInputException;
-import co.fineants.api.global.errors.exception.business.ImageNameEmptyInvalidInputException;
-import co.fineants.api.global.errors.exception.business.ImageSizeExceededInvalidInputException;
-import co.fineants.api.global.errors.exception.business.ImageWriteInvalidInputException;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class AmazonS3RemoteFileUploader implements RemoteFileUploader {
-
-	private static final int MAX_FILE_SIZE = 2 * 1024 * 1024;
-
+	
 	private final String bucketName;
 
 	private final AmazonS3 amazonS3;
@@ -66,41 +58,17 @@ public class AmazonS3RemoteFileUploader implements RemoteFileUploader {
 
 	@Override
 	public String uploadImageFile(MultipartFile multipartFile, String profilePath) {
-		File file = convertMultiPartFileToFile(multipartFile);
-		String key = profilePath + uuidGenerator.generate() + file.getName();
+		ProfileImageFile profileImageFile = new ProfileImageFile(multipartFile);
+		String key = profilePath + uuidGenerator.generate() + profileImageFile.getFileName();
 
-		amazonS3.putObject(new PutObjectRequest(bucketName, key, file)
+		amazonS3.putObject(new PutObjectRequest(bucketName, key, profileImageFile.getFile())
 			.withCannedAcl(CannedAccessControlList.PublicRead));
 		// delete object
 		try {
-			Files.delete(file.toPath());
+			Files.delete(profileImageFile.getFile().toPath());
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
 		return key;
-	}
-
-	private File convertMultiPartFileToFile(MultipartFile file) throws
-		ImageEmptyInvalidInputException,
-		ImageSizeExceededInvalidInputException,
-		ImageNameEmptyInvalidInputException,
-		ImageWriteInvalidInputException {
-		if (file == null || file.isEmpty()) {
-			throw new ImageEmptyInvalidInputException();
-		}
-		if (file.getSize() > MAX_FILE_SIZE) {
-			throw new ImageSizeExceededInvalidInputException(file);
-		}
-		String filename = file.getOriginalFilename();
-		if (filename == null) {
-			throw new ImageNameEmptyInvalidInputException(filename);
-		}
-		File convertedFile = new File(filename);
-		try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
-			fos.write(file.getBytes());
-		} catch (IOException e) {
-			throw new ImageWriteInvalidInputException(convertedFile);
-		}
-		return convertedFile;
 	}
 }
