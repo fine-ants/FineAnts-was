ecad24ad64de8619357cbabcb5c4c431ec786697
yonghwankim-dev <dragonbead95@naver.com>
2025-09-03 11:03:31 +0900

    add StockCsvFormatter

--- src/main/java/co/fineants/api/infra/s3/service/AmazonS3WriteStockService.java
+++ src/main/java/co/fineants/api/infra/s3/service/AmazonS3WriteStockService.java
@@ -2,12 +2,18 @@ package co.fineants.api.infra.s3.service;
 
 import java.util.List;
 
+import org.apache.logging.log4j.util.Strings;
+
 import co.fineants.api.domain.stock.domain.entity.Stock;
 
 public class AmazonS3WriteStockService implements WriteStockService {
 
+	private RemoteFileUploader fileUploader;
+	private String filePath = "local/stock/stocks.csv";
+
 	@Override
 	public void writeStocks(List<Stock> stocks) {
-		
+		// String content = formatter.format(stocks);
+		fileUploader.upload(Strings.EMPTY, filePath);
 	}
 }
