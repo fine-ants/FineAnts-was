b40fb70209a9740075abfaadb8e55aff4faa11a1
yonghwankim-dev <dragonbead95@naver.com>
2025-09-03 13:17:41 +0900

    fix to CsvFormatter

--- src/test/java/co/fineants/api/infra/s3/service/AmazonS3WriteDividendServiceTest.java
+++ src/test/java/co/fineants/api/infra/s3/service/AmazonS3WriteDividendServiceTest.java
@@ -5,13 +5,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import co.fineants.api.domain.dividend.domain.entity.StockDividend;
+
 class AmazonS3WriteDividendServiceTest {
 
 	private WriteDividendService service;
 
 	@BeforeEach
 	void setUp() {
-		DividendCsvFormatter formatter = new DividendCsvFormatter();
+		CsvFormatter<StockDividend> formatter = new CsvFormatter<>(",",
+			new String[] {"id", "dividend", "recordDate", "paymentDate", "stockCode"});
 		RemoteFileUploader fileUploader = Mockito.mock(RemoteFileUploader.class);
 		String filePath = "local/dividend/dividends.csv";
 		service = new AmazonS3WriteDividendService(formatter, fileUploader, filePath);
