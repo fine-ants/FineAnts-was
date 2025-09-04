354589ffb548f0ac6f33d4e6eb7c80b3f2534ef3
yonghwankim-dev <dragonbead95@naver.com>
2025-09-04 15:36:14 +0900

    add method - extractPathFormUrl

--- src/test/java/co/fineants/api/infra/s3/service/AmazonS3DeleteProfileImageFileServiceTest.java
+++ src/test/java/co/fineants/api/infra/s3/service/AmazonS3DeleteProfileImageFileServiceTest.java
@@ -1,5 +1,8 @@
 package co.fineants.api.infra.s3.service;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,11 +31,23 @@ class AmazonS3DeleteProfileImageFileServiceTest extends AbstractContainerBaseTes
 	void delete() {
 		// 이미지 파일 저장하기
 		MultipartFile profileFile = TestDataFactory.createProfileFile();
-		String key = writeService.upload(profileFile);
+		String url = writeService.upload(profileFile);
+		String path = extractPathFromUrl(url);
+		Assertions.assertThat(fetcher.read(path)).isPresent();
 		// 이미지 파일 삭제
-		service.delete(key);
+		service.delete(url);
 		// 이미지 파일이 삭제되었는지 확인
-		Assertions.assertThat(fetcher.read(key)).isEmpty();
+		Assertions.assertThat(fetcher.read(path)).isEmpty();
 	}
 
+	private String extractPathFromUrl(String url) {
+		try {
+			URL u = new URL(url);
+			String path = u.getPath(); // -> "/fineants2024/local/profile/46e5f63c-074a-467c-bccb-06ef9968290eprofile.jpeg"
+			return path.substring(
+				"/fineants2024/".length()); // -> "local/profile/46e5f63c-074a-467c-bccb-06ef9968290eprofile.jpeg"
+		} catch (MalformedURLException e) {
+			throw new IllegalArgumentException("Invalid URL: " + url, e);
+		}
+	}
 }
