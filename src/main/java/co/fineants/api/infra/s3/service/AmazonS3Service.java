803586c8ebc3d26870003e489114f43d98bf928d
yonghwankim-dev <dragonbead95@naver.com>
2025-09-04 13:32:08 +0900

    delete method - upload in AmazonS3Service

--- src/main/java/co/fineants/api/infra/s3/service/AmazonS3Service.java
+++ src/main/java/co/fineants/api/infra/s3/service/AmazonS3Service.java
@@ -1,11 +1,6 @@
 package co.fineants.api.infra.s3.service;
 
-import org.springframework.web.multipart.MultipartFile;
-
-import co.fineants.api.global.errors.exception.business.InvalidInputException;
-
 public interface AmazonS3Service {
-	String upload(MultipartFile multipartFile) throws InvalidInputException;
 
 	void deleteProfileImageFile(String url);
 }
