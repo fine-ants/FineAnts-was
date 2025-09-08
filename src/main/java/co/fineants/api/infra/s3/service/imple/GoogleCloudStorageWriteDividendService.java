1ff7903138938edd040c0a605ae44a8a6679eaf2
yonghwankim-dev <dragonbead95@naver.com>
2025-09-08 16:24:25 +0900

    imple GoogleCloudStorageWriteDividendService

--- src/main/java/co/fineants/api/infra/s3/service/imple/GoogleCloudStorageWriteDividendService.java
+++ src/main/java/co/fineants/api/infra/s3/service/imple/GoogleCloudStorageWriteDividendService.java
@@ -2,17 +2,34 @@ package co.fineants.api.infra.s3.service.imple;
 
 import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import co.fineants.api.domain.dividend.domain.entity.StockDividend;
+import co.fineants.api.global.common.csv.CsvFormatter;
+import co.fineants.api.infra.s3.service.RemoteFileUploader;
 import co.fineants.api.infra.s3.service.WriteDividendService;
 
 public class GoogleCloudStorageWriteDividendService implements WriteDividendService {
+
+	private final CsvFormatter<StockDividend> formatter;
+	private final RemoteFileUploader uploader;
+	private final String dividendPath;
+
+	public GoogleCloudStorageWriteDividendService(CsvFormatter<StockDividend> formatter, RemoteFileUploader uploader,
+		@Value("${gcp.storage.dividend-csv-path}") String dividendPath) {
+		this.formatter = formatter;
+		this.uploader = uploader;
+		this.dividendPath = dividendPath;
+	}
+
 	@Override
 	public void writeDividend(Collection<StockDividend> dividends) {
-
+		writeDividend(dividends.toArray(StockDividend[]::new));
 	}
 
 	@Override
 	public void writeDividend(StockDividend... dividends) {
-
+		String content = formatter.format(dividends);
+		uploader.upload(content, dividendPath);
 	}
 }
--- src/test/java/co/fineants/api/infra/s3/service/imple/GoogleCloudStorageWriteDividendServiceTest.java
+++ src/test/java/co/fineants/api/infra/s3/service/imple/GoogleCloudStorageWriteDividendServiceTest.java
@@ -1,24 +1,41 @@
 package co.fineants.api.infra.s3.service.imple;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import co.fineants.AbstractContainerBaseTest;
+import co.fineants.api.domain.dividend.domain.entity.StockDividend;
+import co.fineants.api.global.common.csv.CsvFormatter;
+import co.fineants.api.infra.s3.service.RemoteFileUploader;
 import co.fineants.api.infra.s3.service.WriteDividendService;
 
 class GoogleCloudStorageWriteDividendServiceTest extends AbstractContainerBaseTest {
 
+	@Autowired
+	private CsvFormatter<StockDividend> formatter;
+
+	@Autowired
+	private RemoteFileUploader uploader;
+
+	@Value("${gcp.storage.dividend-csv-path}")
+	private String dividendPath;
+	private WriteDividendService service;
+
+	@BeforeEach
+	void setUp() {
+		service = new GoogleCloudStorageWriteDividendService(formatter, uploader, dividendPath);
+	}
+
 	@Test
 	void canCreated() {
-		WriteDividendService service = new GoogleCloudStorageWriteDividendService();
-
 		Assertions.assertThat(service).isNotNull();
 	}
 
 	@Test
 	void writeDividend() {
-		WriteDividendService service = new GoogleCloudStorageWriteDividendService();
-
 		service.writeDividend();
 	}
 }
