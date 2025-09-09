109812f03bb109791c712e676a563e31e0f587fe
yonghwankim-dev <dragonbead95@naver.com>
2025-09-09 12:46:00 +0900

    add logging - upload

--- src/main/java/co/fineants/api/infra/s3/service/imple/GoogleCloudStorageRemoteFileUploader.java
+++ src/main/java/co/fineants/api/infra/s3/service/imple/GoogleCloudStorageRemoteFileUploader.java
@@ -2,14 +2,17 @@ package co.fineants.api.infra.s3.service.imple;
 
 import java.nio.charset.StandardCharsets;
 
+import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 
 import co.fineants.api.domain.member.domain.entity.ProfileImageFile;
 import co.fineants.api.infra.s3.service.RemoteFileUploader;
+import lombok.extern.slf4j.Slf4j;
 
 // todo: 구글 클라우드 스토리지 업로더 구현
+@Slf4j
 public class GoogleCloudStorageRemoteFileUploader implements RemoteFileUploader {
 
 	private final Storage storage;
@@ -24,7 +27,8 @@ public class GoogleCloudStorageRemoteFileUploader implements RemoteFileUploader 
 	public void upload(String fileContent, String filePath) {
 		BlobId blobId = BlobId.of(bucketName, filePath);
 		BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
-		storage.create(blobInfo, fileContent.getBytes(StandardCharsets.UTF_8));
+		Blob blob = storage.create(blobInfo, fileContent.getBytes(StandardCharsets.UTF_8));
+		log.info("File uploaded to bucket {} with path {}. Blob info: {}", bucketName, filePath, blob);
 	}
 
 	@Override
