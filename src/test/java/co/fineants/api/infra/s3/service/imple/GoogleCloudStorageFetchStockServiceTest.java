a4876784cf4d1b1ce63510f43b04e882d49432b0
yonghwankim-dev <dragonbead95@naver.com>
2025-09-10 15:57:40 +0900

    update test - fetchStocks

--- src/test/java/co/fineants/api/infra/s3/service/imple/GoogleCloudStorageFetchStockServiceTest.java
+++ src/test/java/co/fineants/api/infra/s3/service/imple/GoogleCloudStorageFetchStockServiceTest.java
@@ -1,15 +1,19 @@
 package co.fineants.api.infra.s3.service.imple;
 
-import java.util.NoSuchElementException;
+import java.util.List;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 import co.fineants.AbstractContainerBaseTest;
+import co.fineants.TestDataFactory;
+import co.fineants.api.domain.stock.domain.entity.Stock;
 import co.fineants.api.infra.s3.service.FetchStockService;
+import co.fineants.api.infra.s3.service.WriteStockService;
 import co.fineants.config.GoogleCloudStorageBucketInitializer;
 import co.fineants.config.GoogleCloudStorageTestConfig;
 
@@ -20,6 +24,17 @@ class GoogleCloudStorageFetchStockServiceTest extends AbstractContainerBaseTest 
 	@Autowired
 	private FetchStockService service;
 
+	@Autowired
+	private WriteStockService writeStockService;
+
+	@BeforeEach
+	void setUp() {
+		Stock samsungStock = TestDataFactory.createSamsungStock();
+		Stock kakaoStock = TestDataFactory.createKakaoStock();
+
+		writeStockService.writeStocks(List.of(samsungStock, kakaoStock));
+	}
+
 	@Test
 	void canCreated() {
 		Assertions.assertThat(service).isNotNull();
@@ -27,9 +42,8 @@ class GoogleCloudStorageFetchStockServiceTest extends AbstractContainerBaseTest 
 
 	@Test
 	void fetchStocks() {
-		Throwable throwable = Assertions.catchThrowable(() -> service.fetchStocks());
+		List<Stock> stocks = service.fetchStocks();
 
-		Assertions.assertThat(throwable)
-			.isInstanceOf(NoSuchElementException.class);
+		Assertions.assertThat(stocks).hasSize(2);
 	}
 }
