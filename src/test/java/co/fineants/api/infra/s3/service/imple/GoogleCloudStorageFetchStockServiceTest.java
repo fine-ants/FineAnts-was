1c778fb01efe37f665039fd4d99ff28acd779d7f
yonghwankim-dev <dragonbead95@naver.com>
2025-09-10 15:48:12 +0900

    add test - canCreated, GoogleCloudStorageFetchStockService

---
+++ src/test/java/co/fineants/api/infra/s3/service/imple/GoogleCloudStorageFetchStockServiceTest.java
@@ -0,0 +1,26 @@
+package co.fineants.api.infra.s3.service.imple;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import co.fineants.AbstractContainerBaseTest;
+import co.fineants.api.infra.s3.service.FetchStockService;
+import co.fineants.config.GoogleCloudStorageBucketInitializer;
+import co.fineants.config.GoogleCloudStorageTestConfig;
+
+@ActiveProfiles(value = {"test", "gcp"}, inheritProfiles = false)
+@ContextConfiguration(classes = {GoogleCloudStorageTestConfig.class, GoogleCloudStorageBucketInitializer.class})
+class GoogleCloudStorageFetchStockServiceTest extends AbstractContainerBaseTest {
+
+	@Autowired
+	private FetchStockService service;
+
+	@Test
+	void canCreated() {
+		Assertions.assertThat(service).isNotNull();
+	}
+
+}
