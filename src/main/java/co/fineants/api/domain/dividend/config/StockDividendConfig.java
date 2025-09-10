51da8b4141b7f6e35e10d8e5d3560eb7dc59f7f5
yonghwankim-dev <dragonbead95@naver.com>
2025-09-10 13:33:21 +0900

    introduce parameter - csvSeparator

--- src/main/java/co/fineants/api/domain/dividend/config/StockDividendConfig.java
+++ src/main/java/co/fineants/api/domain/dividend/config/StockDividendConfig.java
@@ -1,5 +1,6 @@
 package co.fineants.api.domain.dividend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,8 +23,8 @@ public class StockDividendConfig {
 	}
 
 	@Bean
-	public StockDividendCsvParser stockDividendCsvParser(StockDividendCsvLineParser lineParser) {
-		String csvSeparator = ",";
+	public StockDividendCsvParser stockDividendCsvParser(@Value("${csv.stockDividend.delimiter}") String csvSeparator,
+		StockDividendCsvLineParser lineParser) {
 		return new StockDividendCsvParser(csvSeparator, lineParser);
 	}
 }
