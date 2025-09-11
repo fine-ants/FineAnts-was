f20886bf17afd840961341d88abc30b89be233fd
yonghwankim-dev <dragonbead95@naver.com>
2025-09-11 13:52:54 +0900

    add test - read

--- src/test/java/co/fineants/api/infra/s3/service/imple/GoogleCloudStorageRemoteFileFetcherTest.java
+++ src/test/java/co/fineants/api/infra/s3/service/imple/GoogleCloudStorageRemoteFileFetcherTest.java
@@ -1,6 +1,9 @@
 package co.fineants.api.infra.s3.service.imple;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -12,9 +15,12 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.multipart.MultipartFile;
 
 import co.fineants.AbstractContainerBaseTest;
+import co.fineants.TestDataFactory;
 import co.fineants.api.infra.s3.service.RemoteFileFetcher;
+import co.fineants.api.infra.s3.service.WriteProfileImageFileService;
 import co.fineants.config.GoogleCloudStorageBucketInitializer;
 import co.fineants.config.GoogleCloudStorageTestConfig;
 
@@ -24,6 +30,9 @@ class GoogleCloudStorageRemoteFileFetcherTest extends AbstractContainerBaseTest 
 
 	@Autowired
 	private RemoteFileFetcher remoteFileFetcher;
+
+	@Autowired
+	private WriteProfileImageFileService writeProfileImageFileService;
 
 	public static Stream<Arguments> invalidPathSource() {
 		return Stream.of(
@@ -45,5 +54,23 @@ class GoogleCloudStorageRemoteFileFetcherTest extends AbstractContainerBaseTest 
 		Optional<InputStream> inputStream = remoteFileFetcher.read(path);
 
 		Assertions.assertThat(inputStream).isEmpty();
+	}
+
+	@Test
+	void read() throws URISyntaxException, IOException {
+		MultipartFile profileFile = TestDataFactory.createProfileFile();
+		String url = writeProfileImageFileService.upload(profileFile);
+		String path = parseProfilePath(url);
+
+		Optional<InputStream> inputStream = remoteFileFetcher.read(path);
+
+		Assertions.assertThat(inputStream).isPresent();
+		Assertions.assertThat(inputStream.get().readAllBytes()).isEqualTo(profileFile.getBytes());
+	}
+
+	private String parseProfilePath(String url) throws URISyntaxException {
+		URI uri = new URI(url);
+		String fullPath = uri.getPath();
+		return fullPath.substring(fullPath.indexOf("/", 1) + 1);
 	}
 }
