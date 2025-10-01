5c3097c4e29a5034021b3439595c9e12dc8de3ab
yonghwankim-dev <dragonbead95@naver.com>
2025-10-01 12:02:04 +0900

    delete SetupDataLoaderListener

--- src/main/java/co/fineants/api/global/init/SetupDataLoader.java
+++ src/main/java/co/fineants/api/global/init/SetupDataLoader.java
@@ -1,29 +1,37 @@
 package co.fineants.api.global.init;
 
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import co.fineants.api.global.init.properties.MemberProperties;
 import co.fineants.api.global.init.properties.RoleProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Profile(value = {"local", "dev", "release", "production"})
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class SetupDataLoader {
+public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
 	private final RoleSetupDataLoader roleSetupDataLoader;
 	private final RoleProperties roleProperties;
 	private final MemberSetupDataLoader memberSetupDataLoader;
 	private final MemberProperties memberProperties;
 	private final StockSetupDataLoader stockSetupDataLoader;
 	private final StockDividendSetupDataLoader stockDividendSetupDataLoader;
+	private boolean alreadySetup = false;
 
-	@Transactional
-	public void setupResources() {
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		if (alreadySetup) {
+			return;
+		}
 		roleSetupDataLoader.setupRoles(roleProperties);
 		memberSetupDataLoader.setupMembers(memberProperties);
 		stockSetupDataLoader.setupStocks();
 		stockDividendSetupDataLoader.setupStockDividends();
+		alreadySetup = true;
 	}
 }
