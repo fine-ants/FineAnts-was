05944108d90b75b189c35295eee20a32b4ee9950
yonghwankim-dev <dragonbead95@naver.com>
2025-10-10 12:17:25 +0900

    imple fetchDividendEntityIn

--- src/main/java/co/fineants/api/infra/s3/service/imple/AmazonS3FetchDividendService.java
+++ src/main/java/co/fineants/api/infra/s3/service/imple/AmazonS3FetchDividendService.java
@@ -3,6 +3,8 @@ package co.fineants.api.infra.s3.service.imple;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 
@@ -53,6 +55,12 @@ public class AmazonS3FetchDividendService implements FetchDividendService {
 
 	@Override
 	public List<StockDividendTemp> fetchDividendEntityIn(List<Stock> stocks) {
-		return stockDividendCsvParser.parse(fileFetcher.read(dividendPath).orElseThrow());
+		Map<String, Stock> stockMap = stocks.stream()
+			.collect(Collectors.toMap(Stock::getTickerSymbol, stock -> stock));
+		List<StockDividendTemp> stockDividends = stockDividendCsvParser.parse(
+			fileFetcher.read(dividendPath).orElseThrow());
+		return stockDividends.stream()
+			.filter(stockDividend -> stockMap.containsKey(stockDividend.getTickerSymbol()))
+			.toList();
 	}
 }
