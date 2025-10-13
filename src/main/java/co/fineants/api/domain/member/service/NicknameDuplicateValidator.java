2faee755bba0e78f05b82a25d81610350a5cf999
yonghwankim-dev <dragonbead95@naver.com>
2025-10-13 12:55:48 +0900

    imple isDuplicate method

--- src/main/java/co/fineants/api/domain/member/service/NicknameDuplicateValidator.java
+++ src/main/java/co/fineants/api/domain/member/service/NicknameDuplicateValidator.java
@@ -3,8 +3,16 @@ package co.fineants.api.domain.member.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import co.fineants.api.domain.member.repository.MemberRepository;
+
 @Service
 public class NicknameDuplicateValidator {
+
+	private final MemberRepository memberRepository;
+
+	public NicknameDuplicateValidator(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 
 	/**
 	 * 닉네임 중복 검증
@@ -12,8 +20,8 @@ public class NicknameDuplicateValidator {
 	 * @param nickname 닉네임
 	 * @return true: 중복, false: 중복 아님
 	 */
-	@Transactional
+	@Transactional(readOnly = true)
 	public boolean isDuplicate(String nickname) {
-		return true;
+		return memberRepository.findMemberByNickname(nickname).isPresent();
 	}
 }
--- src/test/java/co/fineants/api/domain/member/service/NicknameDuplicateValidatorTest.java
+++ src/test/java/co/fineants/api/domain/member/service/NicknameDuplicateValidatorTest.java
@@ -1,17 +1,43 @@
 package co.fineants.api.domain.member.service;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-class NicknameDuplicateValidatorTest {
+import co.fineants.AbstractContainerBaseTest;
+import co.fineants.TestDataFactory;
+import co.fineants.api.domain.member.repository.MemberRepository;
+
+class NicknameDuplicateValidatorTest extends AbstractContainerBaseTest {
+
+	@Autowired
+	private NicknameDuplicateValidator validator;
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@AfterEach
+	void tearDown() {
+		memberRepository.deleteAll();
+	}
 
 	@Test
 	void isDuplicate_whenDuplicatedNickname_thenReturnTrue() {
-		NicknameDuplicateValidator validator = new NicknameDuplicateValidator();
-		String nickname = "ants1234";
+		memberRepository.save(TestDataFactory.createMember());
+		String nickname = "nemo1234";
 
 		boolean actual = validator.isDuplicate(nickname);
 
 		Assertions.assertThat(actual).isTrue();
+	}
+
+	@Test
+	void isDuplicate_whenNotDuplicatedNickname_thenReturnFalse() {
+		String nickname = "nemo2345";
+
+		boolean actual = validator.isDuplicate(nickname);
+
+		Assertions.assertThat(actual).isFalse();
 	}
 }
