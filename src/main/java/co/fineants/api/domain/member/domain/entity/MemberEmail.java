80e50431b74e6b06bc1acaa287746e5bb3e0d10a
yonghwankim-dev <dragonbead95@naver.com>
2025-10-13 20:08:22 +0900

    add pattern in MemberEmail constructor

--- src/main/java/co/fineants/api/domain/member/domain/entity/MemberEmail.java
+++ src/main/java/co/fineants/api/domain/member/domain/entity/MemberEmail.java
@@ -1,5 +1,10 @@
 package co.fineants.api.domain.member.domain.entity;
 
+import java.util.regex.Pattern;
+
+import org.apache.logging.log4j.util.Strings;
+
+import co.fineants.api.global.errors.exception.business.EmailInvalidInputException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.EqualsAndHashCode;
@@ -17,6 +22,16 @@ public class MemberEmail {
 
 	public MemberEmail(String value) {
 		this.value = value;
+		if (Strings.isBlank(this.value)) {
+			throw new EmailInvalidInputException(this.value);
+		}
+		if (this.value.contains("..")) { // 연속된 마침표는 허용하지 않음
+			throw new EmailInvalidInputException(this.value);
+		}
+		Pattern pattern = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
+		if (!pattern.matcher(this.value).matches()) {
+			throw new EmailInvalidInputException(value);
+		}
 	}
 
 	@Override
--- src/test/java/co/fineants/api/domain/member/domain/entity/MemberEmailTest.java
+++ src/test/java/co/fineants/api/domain/member/domain/entity/MemberEmailTest.java
@@ -2,6 +2,10 @@ package co.fineants.api.domain.member.domain.entity;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import co.fineants.api.global.errors.exception.business.EmailInvalidInputException;
 
 class MemberEmailTest {
 	@Test
@@ -9,5 +13,14 @@ class MemberEmailTest {
 		MemberEmail memberEmail = new MemberEmail("ant1234@gmail.com");
 
 		Assertions.assertThat(memberEmail).isNotNull();
+	}
+
+	@ParameterizedTest
+	@MethodSource(value = "co.fineants.TestDataProvider#invalidEmailValues")
+	void newInstance_whenInvalidEmail_thenThrowException(String value) {
+		Throwable throwable = Assertions.catchThrowable(() -> new MemberEmail(value));
+
+		Assertions.assertThat(throwable)
+			.isInstanceOf(EmailInvalidInputException.class);
 	}
 }
