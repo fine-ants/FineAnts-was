bbeff0b7e102cbbba631e0d6209fdfa73aceeae9
yonghwankim-dev <dragonbead95@naver.com>
2025-10-13 13:26:14 +0900

    create test - NicknameFactory#create()

---
+++ src/test/java/co/fineants/TestDataProvider.java
@@ -0,0 +1,18 @@
+package co.fineants;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+public class TestDataProvider {
+
+	public static Stream<Arguments> validNicknameValues() {
+		return Stream.of(
+			Arguments.of("User123"),
+			Arguments.of("AlphaBeta"),
+			Arguments.of("CoolGuy99"),
+			Arguments.of("JaneDoe"),
+			Arguments.of("개미1234")
+		);
+	}
+}
