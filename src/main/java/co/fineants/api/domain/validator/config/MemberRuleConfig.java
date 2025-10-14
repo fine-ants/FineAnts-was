7b3344bbb50ad8758814ab20f839786eb8d05dfe
yonghwankim-dev <dragonbead95@naver.com>
2025-10-14 13:21:11 +0900

    delete MemberEmailFactory class

--- src/main/java/co/fineants/api/domain/validator/config/MemberRuleConfig.java
+++ src/main/java/co/fineants/api/domain/validator/config/MemberRuleConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import co.fineants.api.domain.member.properties.NicknameProperties;
 import co.fineants.api.domain.member.service.EmailDuplicateValidator;
 import co.fineants.api.domain.member.service.NicknameDuplicateValidator;
-import co.fineants.api.domain.member.service.factory.MemberEmailFactory;
 import co.fineants.api.domain.member.service.factory.NicknameFactory;
 import co.fineants.api.domain.validator.domain.MemberValidationRule;
 import co.fineants.api.domain.validator.domain.member.EmailDuplicationRule;
@@ -22,8 +21,8 @@ import co.fineants.api.domain.validator.domain.member.SignUpValidator;
 public class MemberRuleConfig {
 
 	@Bean
-	public EmailFormatRule emailFormatRule(MemberEmailFactory factory) {
-		return new EmailFormatRule(factory);
+	public EmailFormatRule emailFormatRule() {
+		return new EmailFormatRule();
 	}
 
 	@Bean
