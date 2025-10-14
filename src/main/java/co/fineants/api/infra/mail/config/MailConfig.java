fe7d67d41e546552478012734581d70fc696f447
yonghwankim-dev <dragonbead95@naver.com>
2025-10-14 15:38:33 +0900

    move MailConfig to co.fineants.api.infra.mail.config package

--- src/main/java/co/fineants/api/infra/mail/config/MailConfig.java
+++ src/main/java/co/fineants/api/infra/mail/config/MailConfig.java
@@ -1,4 +1,4 @@
-package co.fineants.api.domain.member.config;
+package co.fineants.api.infra.mail.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
