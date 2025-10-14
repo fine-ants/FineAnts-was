cedb70ff869db16574813862eba60a2949d116ac
yonghwankim-dev <dragonbead95@naver.com>
2025-10-14 15:39:29 +0900

    move spring bean declaration to MailConfig

--- src/main/java/co/fineants/api/infra/mail/config/MailConfig.java
+++ src/main/java/co/fineants/api/infra/mail/config/MailConfig.java
@@ -3,10 +3,13 @@ package co.fineants.api.infra.mail.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import co.fineants.api.domain.member.service.ExchangeRateErrorMailHtmlRender;
 import co.fineants.api.domain.member.service.VerifyCodeMailHtmlRender;
+import co.fineants.api.infra.mail.ExchangeRateErrorMimeMessageFactory;
+import co.fineants.api.infra.mail.VerifyCodeMimeMessageFactory;
 
 @Configuration
 public class MailConfig {
@@ -22,5 +25,20 @@ public class MailConfig {
 		@Value("${mail.templates.path.exchange-rate-error}") String templateName,
 		SpringTemplateEngine engine) {
 		return new ExchangeRateErrorMailHtmlRender(templateName, engine);
+	}
+
+	@Bean
+	public VerifyCodeMimeMessageFactory verifyCodeMimeMessageFactory(VerifyCodeMailHtmlRender render,
+		JavaMailSender sender) {
+		String subject = "Finants 회원가입 인증 코드";
+		return new VerifyCodeMimeMessageFactory(render, sender, subject);
+	}
+
+	@Bean
+	public ExchangeRateErrorMimeMessageFactory exchangeRateErrorMimeMessageFactory(
+		ExchangeRateErrorMailHtmlRender render,
+		JavaMailSender sender) {
+		String subject = "환율 API 서버 오류";
+		return new ExchangeRateErrorMimeMessageFactory(render, sender, subject);
 	}
 }
