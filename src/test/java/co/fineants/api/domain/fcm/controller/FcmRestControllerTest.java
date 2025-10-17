682a344294cc60fa350925c60e58ede5f29cc296
yonghwankim-dev <dragonbead95@naver.com>
2025-10-17 11:26:01 +0900

    fix test - deleteFcmToken

--- src/test/java/co/fineants/api/domain/fcm/controller/FcmRestControllerTest.java
+++ src/test/java/co/fineants/api/domain/fcm/controller/FcmRestControllerTest.java
@@ -2,17 +2,12 @@ package co.fineants.api.domain.fcm.controller;
 
 import static co.fineants.api.global.success.FcmSuccessCode.*;
 import static org.hamcrest.Matchers.*;
-import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,9 +16,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import co.fineants.AbstractContainerBaseTest;
 import co.fineants.TestDataFactory;
 import co.fineants.api.domain.fcm.domain.dto.request.FcmRegisterRequest;
-import co.fineants.api.domain.fcm.domain.dto.response.FcmDeleteResponse;
+import co.fineants.api.domain.fcm.domain.entity.FcmToken;
+import co.fineants.api.domain.fcm.repository.FcmRepository;
 import co.fineants.api.domain.fcm.service.FcmService;
 import co.fineants.api.global.util.ObjectMapperUtil;
+import co.fineants.member.domain.Member;
 import co.fineants.member.domain.MemberRepository;
 
 class FcmRestControllerTest extends AbstractContainerBaseTest {
@@ -36,6 +33,9 @@ class FcmRestControllerTest extends AbstractContainerBaseTest {
 
 	@Autowired
 	private MemberRepository memberRepository;
+
+	@Autowired
+	private FcmRepository fcmRepository;
 
 	private MockMvc mockMvc;
 
@@ -66,34 +66,35 @@ class FcmRestControllerTest extends AbstractContainerBaseTest {
 	@Test
 	void registerToken_whenInvalidFcmToken_thenResponse400Error() throws Exception {
 		// given
-		Map<String, String> body = new HashMap<>();
-		body.put("fcmToken", null);
+		memberRepository.save(TestDataFactory.createMember());
+		FcmRegisterRequest request = new FcmRegisterRequest(null);
 
 		// when
 		mockMvc.perform(post("/api/fcm/tokens")
-				.content(ObjectMapperUtil.serialize(body))
-				.contentType(MediaType.APPLICATION_JSON))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(ObjectMapperUtil.serialize(request)))
 			.andExpect(status().isBadRequest())
-			.andExpect(jsonPath("code").value(equalTo(400)))
-			.andExpect(jsonPath("status").value(equalTo("Bad Request")))
-			.andExpect(jsonPath("message").value(equalTo("잘못된 입력형식입니다")));
+			.andExpect(jsonPath("code").value(equalTo(HttpStatus.BAD_REQUEST.value())))
+			.andExpect(jsonPath("status").value(equalTo(HttpStatus.BAD_REQUEST.getReasonPhrase())))
+			.andExpect(jsonPath("message").value(equalTo("잘못된 입력형식입니다")))
+			.andExpect(jsonPath("data[*].field", containsInAnyOrder("fcmToken")))
+			.andExpect(jsonPath("data[*].defaultMessage", containsInAnyOrder("FCM 토큰은 필수 정보입니다")));
 	}
 
 	@DisplayName("사용자는 FCM 토큰을 삭제한다")
 	@Test
-	void deleteToken() throws Exception {
+	void deleteFcmToken() throws Exception {
 		// given
-		Long fcmTokenId = 1L;
-		given(mockedFcmService.deleteToken(ArgumentMatchers.anyLong()))
-			.willReturn(FcmDeleteResponse.builder()
-				.fcmTokenId(fcmTokenId)
-				.build());
+		Member member = memberRepository.save(TestDataFactory.createMember());
+		FcmToken fcmToken = FcmToken.create(member, "fcmToken");
+		fcmRepository.save(fcmToken);
 
 		// when & then
-		mockMvc.perform(delete("/api/fcm/tokens/{fcmTokenId}", fcmTokenId))
+		mockMvc.perform(delete("/api/fcm/tokens/{fcmTokenId}", fcmToken.getId()))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("code").value(equalTo(200)))
-			.andExpect(jsonPath("status").value(equalTo("OK")))
-			.andExpect(jsonPath("message").value(equalTo("FCM 토큰을 성공적으로 삭제하였습니다")));
+			.andExpect(jsonPath("code").value(equalTo(HttpStatus.OK.value())))
+			.andExpect(jsonPath("status").value(equalTo(HttpStatus.OK.getReasonPhrase())))
+			.andExpect(jsonPath("message").value(equalTo(OK_DELETE_FCM.getMessage())))
+			.andExpect(jsonPath("data").value(nullValue()));
 	}
 }
