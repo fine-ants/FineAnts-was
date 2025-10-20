b614b21b535d9ac9f200a48853ec8c7b7bac52fd
yonghwankim-dev <dragonbead95@naver.com>
2025-10-20 15:42:43 +0900

    delete method - upload
    - 별도의 객체인 UploadMemberProfileImageFile 객체로 이동

--- src/main/java/co/fineants/member/application/SignupService.java
+++ src/main/java/co/fineants/member/application/SignupService.java
@@ -1,17 +1,10 @@
 package co.fineants.member.application;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 import co.fineants.api.domain.validator.domain.member.SignUpValidator;
-import co.fineants.api.global.errors.exception.business.ImageEmptyInvalidInputException;
-import co.fineants.api.global.errors.exception.business.InvalidInputException;
-import co.fineants.api.global.errors.exception.business.MemberProfileUploadException;
 import co.fineants.api.infra.s3.service.DeleteProfileImageFileService;
-import co.fineants.api.infra.s3.service.WriteProfileImageFileService;
 import co.fineants.member.domain.Member;
 import co.fineants.member.domain.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +15,6 @@ public class SignupService {
 
 	private final SignUpValidator signUpValidator;
 	private final MemberRepository memberRepository;
-	private final WriteProfileImageFileService writeProfileImageFileService;
 	private final DeleteProfileImageFileService deleteProfileImageFileService;
 
 	@Transactional
@@ -31,16 +23,6 @@ public class SignupService {
 		signUpValidator.validate(member);
 		// 회원 저장
 		memberRepository.save(member);
-	}
-
-	public Optional<String> upload(MultipartFile file) throws MemberProfileUploadException {
-		try {
-			return Optional.of(writeProfileImageFileService.upload(file));
-		} catch (ImageEmptyInvalidInputException e) {
-			return Optional.empty();
-		} catch (InvalidInputException e) {
-			throw new MemberProfileUploadException(file, e);
-		}
 	}
 
 	public void deleteProfileImageFile(String profileUrl) {
