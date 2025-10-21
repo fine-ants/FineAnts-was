5991dfc095d3eb9edb242742c0d072e417a190e6
yonghwankim-dev <dragonbead95@naver.com>
2025-10-21 12:23:59 +0900

    add test - VerifyCodeTest

---
+++ src/main/java/co/fineants/member/application/VerifyCode.java
@@ -0,0 +1,22 @@
+package co.fineants.member.application;
+
+import org.springframework.stereotype.Service;
+
+import co.fineants.api.global.errors.exception.business.VerifyCodeInvalidInputException;
+import co.fineants.member.domain.VerifyCodeRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class VerifyCode {
+
+	private final VerifyCodeRepository repository;
+
+	public void verifyCode(String email, String code) {
+		String verifyCode = repository.get(email)
+			.orElseThrow(() -> new VerifyCodeInvalidInputException(code));
+		if (!verifyCode.equals(code)) {
+			throw new VerifyCodeInvalidInputException(code);
+		}
+	}
+}
--- src/test/java/co/fineants/member/application/SignupVerificationServiceTest.java
+++ src/test/java/co/fineants/member/application/SignupVerificationServiceTest.java
@@ -1,8 +1,5 @@
 package co.fineants.member.application;
 
-import static org.assertj.core.api.Assertions.*;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
@@ -11,9 +8,7 @@ import org.mockito.internal.verification.Times;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import co.fineants.AbstractContainerBaseTest;
-import co.fineants.api.global.errors.exception.business.VerifyCodeInvalidInputException;
 import co.fineants.api.infra.mail.EmailService;
-import co.fineants.member.application.SignupVerificationService;
 import co.fineants.member.domain.VerifyCodeRepository;
 import jakarta.mail.internet.MimeMessage;
 
@@ -40,32 +35,4 @@ class SignupVerificationServiceTest extends AbstractContainerBaseTest {
 			.should(new Times(1))
 			.sendEmail(ArgumentMatchers.any(MimeMessage.class));
 	}
-
-	@DisplayName("사용자는 검증코드를 제출하여 검증코드가 일치하는지 검사한다")
-	@Test
-	void checkVerifyCode() {
-		// given
-		String email = "dragonbead95@naver.com";
-		String code = "123456";
-		verifyCodeRedisRepository.save(email, code);
-		// when & then
-		Assertions.assertDoesNotThrow(() -> service.verifyCode(email, code));
-	}
-
-	@DisplayName("사용자는 매치되지 않은 검증 코드를 전달하며 검사를 요청했을때 예외가 발생한다")
-	@Test
-	void checkVerifyCode_whenNotMatchVerifyCode_thenThrowException() {
-		// given
-		String email = "dragonbead95@naver.com";
-		String code = "234567";
-
-		// when
-		Throwable throwable = catchThrowable(() -> service.verifyCode(email, code));
-
-		// then
-		assertThat(throwable)
-			.isInstanceOf(VerifyCodeInvalidInputException.class)
-			.hasMessage("234567");
-	}
-
 }
