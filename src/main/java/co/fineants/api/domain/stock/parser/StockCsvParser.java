52d43a8f743adefd0f7c12ec9754a76a9cf5a103
yonghwankim-dev <dragonbead95@naver.com>
2025-10-22 11:53:55 +0900

    delete StockConfig

--- src/main/java/co/fineants/api/domain/stock/parser/StockCsvParser.java
+++ src/main/java/co/fineants/api/domain/stock/parser/StockCsvParser.java
@@ -6,16 +6,20 @@ import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import co.fineants.stock.domain.Stock;
 import lombok.extern.slf4j.Slf4j;
 
+@Component
 @Slf4j
 public class StockCsvParser {
 
 	private final String csvSeparator;
 	private final StockCsvLineParser stockCsvLineParser;
 
-	public StockCsvParser(String csvSeparator, StockCsvLineParser stockCsvLineParser) {
+	public StockCsvParser(@Value("${csv.stock.delimiter}") String csvSeparator, StockCsvLineParser stockCsvLineParser) {
 		this.csvSeparator = csvSeparator;
 		this.stockCsvLineParser = stockCsvLineParser;
 	}
