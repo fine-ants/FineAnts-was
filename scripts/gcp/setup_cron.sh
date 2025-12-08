b26eb36dc14c53cbb22b84f637c1e6f672a6553a
yonghwankim-dev <dragonbead95@naver.com>
2025-12-08 16:30:48 +0900

    add backup log data script and schedule cron job

--- scripts/gcp/setup_cron.sh
+++ scripts/gcp/setup_cron.sh
@@ -16,6 +16,7 @@ fi
 # 스크립트 경로 정의
 BACKUP_MYSQL_SCRIPT_PATH="$GCP_DEPLOY_PATH/scripts/gcp/backup_mysql_data.sh"
 echo "Backup MySQL script path: $BACKUP_MYSQL_SCRIPT_PATH"
+BACKUP_LOG_SCRIPT_PATH="$GCP_DEPLOY_PATH/scripts/gcp/backup_log_data.sh"
 # ex) LOGS_SCRIPT_PATH="$DEPLOY_BASE_PATH/send_logs_to_gcs.sh" # 필요시 추가
 
 CRON_ENV_FILE="$ENV_FILE"
@@ -25,7 +26,8 @@ echo "Crontab environment file path: $CRON_ENV_FILE"
 # 형식: "[분] [시] [일] [월] [요일] [실행 명령어]"
 CRON_JOBS=(
     # 1. MySQL 백업 스크립트
-    "10 0 * * * ENV_FILE=$CRON_ENV_FILE /bin/bash $BACKUP_MYSQL_SCRIPT_PATH"
+    "10 0 * * * ENV_FILE=$CRON_ENV_FILE /bin/bash $BACKUP_MYSQL_SCRIPT_PATH",
+    "0 0 * * * ENV_FILE=$CRON_ENV_FILE /bin/bash $BACKUP_LOG_SCRIPT_PATH"
 )
 
 # =========================================================
