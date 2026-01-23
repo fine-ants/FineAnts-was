4459304ce2a25967dfddd73331f68107f9306bcc
yonghwankim-dev <dragonbead95@naver.com>
2026-01-23 15:28:16 +0900

    add test - fetchPriceBy

--- src/test/java/co/fineants/api/domain/kis/repository/CurrentPriceRedisHashRepositoryTest.java
+++ src/test/java/co/fineants/api/domain/kis/repository/CurrentPriceRedisHashRepositoryTest.java
@@ -1,11 +1,14 @@
 package co.fineants.api.domain.kis.repository;
 
+import java.util.Optional;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import co.fineants.AbstractContainerBaseTest;
+import co.fineants.api.domain.common.money.Money;
 
 class CurrentPriceRedisHashRepositoryTest extends AbstractContainerBaseTest {
 
@@ -27,5 +30,20 @@ class CurrentPriceRedisHashRepositoryTest extends AbstractContainerBaseTest {
 		// then
 		Assertions.assertThat(template.opsForHash().get(CurrentPriceRedisHashRepository.KEY, tickerSymbol))
 			.isEqualTo(String.valueOf(price));
+	}
+
+	@Test
+	void fetchPriceBy() {
+		// given
+		String tickerSymbol = "005930";
+		long price = 50000L;
+		repository.savePrice(tickerSymbol, price);
+
+		// when
+		Optional<Money> currentPrice = repository.fetchPriceBy(tickerSymbol);
+
+		// then
+		Assertions.assertThat(currentPrice).isPresent();
+		Assertions.assertThat(currentPrice.orElseThrow()).isEqualTo(Money.won(price));
 	}
 }
