d6f2c8296a7a4b5c79201cbd01c52c8d69387837
yonghwankim-dev <dragonbead95@naver.com>
2026-01-25 13:45:41 +0900

    delete method - create

--- src/main/java/co/fineants/stock/presentation/dto/response/StockResponse.java
+++ src/main/java/co/fineants/stock/presentation/dto/response/StockResponse.java
@@ -2,9 +2,6 @@ package co.fineants.stock.presentation.dto.response;
 
 import java.util.List;
 
-import co.fineants.api.domain.common.money.Bank;
-import co.fineants.api.domain.common.money.Currency;
-import co.fineants.api.domain.common.money.Expression;
 import co.fineants.api.domain.common.money.Money;
 import co.fineants.api.domain.common.money.Percentage;
 import co.fineants.stock.domain.Market;
@@ -33,26 +30,4 @@ public class StockResponse {
 	private Money annualDividend;
 	private Percentage annualDividendYield;
 	private List<Integer> dividendMonths;
-
-	public static StockResponse create(String stockCode, String tickerSymbol, String companyName,
-		String companyNameEng, Market market, Expression currentPrice, Expression dailyChange,
-		Percentage dailyChangeRate, String sector, Expression annualDividend, Percentage annualDividendYield,
-		List<Integer> dividendMonths) {
-		Bank bank = Bank.getInstance();
-		Currency to = Currency.KRW;
-		return StockResponse.builder()
-			.stockCode(stockCode)
-			.tickerSymbol(tickerSymbol)
-			.companyName(companyName)
-			.companyNameEng(companyNameEng)
-			.market(market)
-			.currentPrice(currentPrice.reduce(bank, to))
-			.dailyChange(dailyChange.reduce(bank, to))
-			.dailyChangeRate(dailyChangeRate)
-			.sector(sector)
-			.annualDividend(annualDividend.reduce(bank, to))
-			.annualDividendYield(annualDividendYield)
-			.dividendMonths(dividendMonths)
-			.build();
-	}
 }
