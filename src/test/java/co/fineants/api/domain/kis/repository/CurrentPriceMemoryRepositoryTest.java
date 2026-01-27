d98ca7042f6417b8a3f4bbc51f66b4a340b5f19e
yonghwankim-dev <dragonbead95@naver.com>
2026-01-27 14:09:50 +0900

    add test - savePrice(KisCurrentPrice...)

--- src/test/java/co/fineants/api/domain/kis/repository/CurrentPriceMemoryRepositoryTest.java
+++ src/test/java/co/fineants/api/domain/kis/repository/CurrentPriceMemoryRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import co.fineants.AbstractContainerBaseTest;
+import co.fineants.api.domain.kis.client.KisCurrentPrice;
 import co.fineants.api.domain.kis.domain.CurrentPriceRedisEntity;
 
 class CurrentPriceMemoryRepositoryTest extends AbstractContainerBaseTest {
@@ -49,6 +50,33 @@ class CurrentPriceMemoryRepositoryTest extends AbstractContainerBaseTest {
 		Assertions.assertThat(currentPriceMemoryRepository.fetchPriceBy("  ")).isEmpty();
 	}
 
+	@DisplayName("savePrice - KisCurrentPrice 배열로 현재가 저장")
+	@Test
+	void savePrice_withKisCurrentPriceArray() {
+		// given
+		String tickerSymbol1 = "005930";
+		long price1 = 60000L;
+		String tickerSymbol2 = "000660";
+		long price2 = 90000L;
+
+		// when
+		currentPriceMemoryRepository.savePrice(
+			KisCurrentPrice.create(tickerSymbol1, price1),
+			KisCurrentPrice.create(tickerSymbol2, price2)
+		);
+
+		// then
+		CurrentPriceRedisEntity actual1 = currentPriceMemoryRepository.fetchPriceBy(tickerSymbol1).orElseThrow();
+		Assertions.assertThat(actual1)
+			.hasFieldOrPropertyWithValue("tickerSymbol", tickerSymbol1)
+			.hasFieldOrPropertyWithValue("price", price1);
+
+		CurrentPriceRedisEntity actual2 = currentPriceMemoryRepository.fetchPriceBy(tickerSymbol2).orElseThrow();
+		Assertions.assertThat(actual2)
+			.hasFieldOrPropertyWithValue("tickerSymbol", tickerSymbol2)
+			.hasFieldOrPropertyWithValue("price", price2);
+	}
+
 	@DisplayName("fetchPriceBy - 존재하지 않는 티커 심볼로 조회 시 빈 Optional 반환")
 	@Test
 	void fetchPriceBy_whenTickerSymbolDoesNotExist_thenReturnEmptyOptional() {
@@ -73,5 +101,24 @@ class CurrentPriceMemoryRepositoryTest extends AbstractContainerBaseTest {
 		Assertions.assertThat(result2).isEmpty();
 		Assertions.assertThat(result3).isEmpty();
 		Assertions.assertThat(result4).isEmpty();
+	}
+
+	@DisplayName("fetchPriceBy - 티커 심볼로 현재가 조회")
+	@Test
+	void fetchPriceBy() {
+		// given
+		String tickerSymbol = "005930";
+		long price = 50000L;
+		currentPriceMemoryRepository.savePrice(tickerSymbol, price);
+
+		// when
+		Optional<CurrentPriceRedisEntity> entity = currentPriceMemoryRepository.fetchPriceBy(tickerSymbol);
+
+		// then
+		Assertions.assertThat(entity).isPresent();
+		CurrentPriceRedisEntity actual = entity.orElseThrow();
+		Assertions.assertThat(actual)
+			.hasFieldOrPropertyWithValue("tickerSymbol", tickerSymbol)
+			.hasFieldOrPropertyWithValue("price", price);
 	}
 }
