a709d5f59194d60d636c28ff1308b2a691e09b2b
yonghwankim-dev <dragonbead95@naver.com>
2026-01-28 14:04:00 +0900

    add test - savePrice(String, long)

--- src/test/java/co/fineants/api/domain/kis/repository/ClosingPriceRedisHashRepositoryTest.java
+++ src/test/java/co/fineants/api/domain/kis/repository/ClosingPriceRedisHashRepositoryTest.java
@@ -1,17 +1,30 @@
 package co.fineants.api.domain.kis.repository;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import co.fineants.AbstractContainerBaseTest;
+import co.fineants.api.domain.common.money.Money;
 
 class ClosingPriceRedisHashRepositoryTest extends AbstractContainerBaseTest {
 
 	@Autowired
 	private ClosingPriceRedisHashRepository repository;
 
+	@DisplayName("savePrice - TickerSymbol과 Price를 받아서 종가를 저장한다.")
 	@Test
-	void savePrice() {
+	void savePrice_TickerSymbolAndPrice_SaveClosingPrice() {
+		// given
+		String tickerSymbol = "005930";
+		long price = 15000L;
 
+		// when
+		repository.savePrice(tickerSymbol, price);
+
+		// then
+		Assertions.assertThat(repository.fetchPrice(tickerSymbol).orElseThrow())
+			.isEqualTo(Money.won(price));
 	}
 }
