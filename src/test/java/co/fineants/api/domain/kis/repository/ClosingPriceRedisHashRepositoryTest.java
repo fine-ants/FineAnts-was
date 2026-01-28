218867fbf18536e00fcb28e5943984d90ec77b9d
yonghwankim-dev <dragonbead95@naver.com>
2026-01-28 13:59:47 +0900

    create test class

---
+++ src/test/java/co/fineants/api/domain/kis/repository/ClosingPriceRedisHashRepositoryTest.java
@@ -0,0 +1,17 @@
+package co.fineants.api.domain.kis.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import co.fineants.AbstractContainerBaseTest;
+
+class ClosingPriceRedisHashRepositoryTest extends AbstractContainerBaseTest {
+
+	@Autowired
+	private ClosingPriceRedisHashRepository repository;
+
+	@Test
+	void savePrice() {
+
+	}
+}
