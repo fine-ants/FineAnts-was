16641539ebdc34d313cde6e37a3a6c73a0a9c063
yonghwankim-dev <dragonbead95@naver.com>
2026-02-02 14:40:23 +0900

    add test - calculateDailyChange negative case

--- src/test/java/co/fineants/stock/domain/StockPriceCalculatorTest.java
+++ src/test/java/co/fineants/stock/domain/StockPriceCalculatorTest.java
@@ -32,4 +32,20 @@ class StockPriceCalculatorTest {
 		// then
 		Assertions.assertThat(dailyChange).isEqualTo(Money.won(200));
 	}
+
+	@DisplayName("일일 변동액 계산 - 음수 일일 변동 금액을 올바르게 계산한다.")
+	@Test
+	void calculateDailyChange_whenCurrentPriceIsLessThanClosingPrice_thenReturnNegativeDailyChange() {
+		// given
+		PriceCalculator calculator = new StockPriceCalculator();
+		Money currentPrice = Money.won(800);
+		Money closingPrice = Money.won(1000);
+
+		// when
+		Money dailyChange = calculator.calculateDailyChange(currentPrice, closingPrice);
+
+		// then
+		Assertions.assertThat(dailyChange).isEqualTo(Money.won(-200));
+	}
+
 }
