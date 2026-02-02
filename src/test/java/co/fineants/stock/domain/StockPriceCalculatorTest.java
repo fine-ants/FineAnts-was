527221dce7c336347c2c0e252db77c401dd38a26
yonghwankim-dev <dragonbead95@naver.com>
2026-02-02 14:44:40 +0900

    add test - calculateDailyChange zero case

--- src/test/java/co/fineants/stock/domain/StockPriceCalculatorTest.java
+++ src/test/java/co/fineants/stock/domain/StockPriceCalculatorTest.java
@@ -48,4 +48,19 @@ class StockPriceCalculatorTest {
 		Assertions.assertThat(dailyChange).isEqualTo(Money.won(-200));
 	}
 
+	@DisplayName("일일 변동액 계산 - 현재가와 종가가 동일한 경우는 0 값으로 계산한다.")
+	@Test
+	void calculateDailyChange_whenCurrentPriceIsEqualToClosingPrice_thenReturnZeroDailyChange() {
+		// given
+		PriceCalculator calculator = new StockPriceCalculator();
+		Money currentPrice = Money.won(1000);
+		Money closingPrice = Money.won(1000);
+
+		// when
+		Money dailyChange = calculator.calculateDailyChange(currentPrice, closingPrice);
+
+		// then
+		Assertions.assertThat(dailyChange).isEqualTo(Money.won(0));
+	}
+
 }
