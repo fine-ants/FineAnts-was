106dd2dc58d522d7a638d791ceef63f4dd54b10c
yonghwankim-dev <dragonbead95@naver.com>
2026-02-02 15:52:40 +0900

    delete @NotNull annotation

--- src/main/java/co/fineants/stock/domain/PriceCalculator.java
+++ src/main/java/co/fineants/stock/domain/PriceCalculator.java
@@ -1,8 +1,7 @@
 package co.fineants.stock.domain;
 
 import co.fineants.api.domain.common.money.Expression;
-import jakarta.validation.constraints.NotNull;
 
 public interface PriceCalculator {
-	Expression calculateDailyChange(@NotNull Expression currentPrice, @NotNull Expression closingPrice);
+	Expression calculateDailyChange(Expression currentPrice, Expression closingPrice);
 }
--- src/main/java/co/fineants/stock/domain/StockPriceCalculator.java
+++ src/main/java/co/fineants/stock/domain/StockPriceCalculator.java
@@ -1,11 +1,10 @@
 package co.fineants.stock.domain;
 
 import co.fineants.api.domain.common.money.Expression;
-import jakarta.validation.constraints.NotNull;
 
 public class StockPriceCalculator implements PriceCalculator {
 	@Override
-	public Expression calculateDailyChange(@NotNull Expression currentPrice, @NotNull Expression closingPrice) {
+	public Expression calculateDailyChange(Expression currentPrice, Expression closingPrice) {
 		return currentPrice.minus(closingPrice);
 	}
 }
