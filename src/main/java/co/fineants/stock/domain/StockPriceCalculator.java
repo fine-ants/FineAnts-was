72c698c899f8859d10205debb8d7e0e1fe3a7932
yonghwankim-dev <dragonbead95@naver.com>
2026-02-02 15:53:38 +0900

    add Objects.requireNonNull code

--- src/main/java/co/fineants/stock/domain/StockPriceCalculator.java
+++ src/main/java/co/fineants/stock/domain/StockPriceCalculator.java
@@ -1,10 +1,14 @@
 package co.fineants.stock.domain;
 
+import java.util.Objects;
+
 import co.fineants.api.domain.common.money.Expression;
 
 public class StockPriceCalculator implements PriceCalculator {
 	@Override
 	public Expression calculateDailyChange(Expression currentPrice, Expression closingPrice) {
+		Objects.requireNonNull(currentPrice, "Current price must not be null");
+		Objects.requireNonNull(closingPrice, "Closing price must not be null");
 		return currentPrice.minus(closingPrice);
 	}
 }
