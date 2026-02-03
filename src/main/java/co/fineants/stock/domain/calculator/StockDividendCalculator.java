ce316b726c66805cccc98f27b622c8df3c2d254d
yonghwankim-dev <dragonbead95@naver.com>
2026-02-03 13:27:19 +0900

    add test - null case, calculateDividendMonths

--- src/main/java/co/fineants/stock/domain/calculator/StockDividendCalculator.java
+++ src/main/java/co/fineants/stock/domain/calculator/StockDividendCalculator.java
@@ -2,12 +2,15 @@ package co.fineants.stock.domain.calculator;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 import co.fineants.stock.domain.StockDividend;
 
 public class StockDividendCalculator implements DividendCalculator {
 	@Override
 	public List<Integer> calculateDividendMonths(List<StockDividend> dividends, LocalDate baseDate) {
+		Objects.requireNonNull(dividends, "dividends must not be null");
+		Objects.requireNonNull(baseDate, "baseDate must not be null");
 		return null;
 	}
 }
--- src/test/java/co/fineants/stock/domain/calculator/StockDividendCalculatorTest.java
+++ src/test/java/co/fineants/stock/domain/calculator/StockDividendCalculatorTest.java
@@ -1,8 +1,14 @@
 package co.fineants.stock.domain.calculator;
 
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import co.fineants.stock.domain.StockDividend;
 
 class StockDividendCalculatorTest {
 
@@ -14,5 +20,19 @@ class StockDividendCalculatorTest {
 
 		// then
 		Assertions.assertThat(calculator).isNotNull();
+	}
+
+	@DisplayName("배당 월 리스트 계산 - 매개변수가 null이면 예외를 던진다")
+	@Test
+	void calculateDividendMonths_ThrowsException_WhenDividendsIsNull() {
+		// given
+		DividendCalculator calculator = new StockDividendCalculator();
+		List<StockDividend> dividends = Collections.emptyList();
+		LocalDate baseDate = LocalDate.of(2023, 6, 1);
+		// when & then
+		Assertions.assertThatThrownBy(() -> calculator.calculateDividendMonths(null, baseDate))
+			.isInstanceOf(NullPointerException.class);
+		Assertions.assertThatThrownBy(() -> calculator.calculateDividendMonths(dividends, null))
+			.isInstanceOf(NullPointerException.class);
 	}
 }
