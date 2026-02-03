f80b756579635ace024d7e7486e907718eeb3670
yonghwankim-dev <dragonbead95@naver.com>
2026-02-03 13:55:37 +0900

    refactor SearchStock - use DividendCalculator

--- src/main/java/co/fineants/stock/domain/calculator/StockDividendCalculator.java
+++ src/main/java/co/fineants/stock/domain/calculator/StockDividendCalculator.java
@@ -5,8 +5,11 @@ import java.time.Month;
 import java.util.List;
 import java.util.Objects;
 
+import org.springframework.stereotype.Component;
+
 import co.fineants.stock.domain.StockDividend;
 
+@Component
 public class StockDividendCalculator implements DividendCalculator {
 	@Override
 	public List<Integer> calculateDividendMonths(List<StockDividend> dividends, LocalDate baseDate) {
