fec5ccf8761b01db28557067027bfdfc60fd3a68
yonghwankim-dev <dragonbead95@naver.com>
2026-02-14 15:33:27 +0900

    change sync to async event

--- src/main/java/co/fineants/stock/application/StockScheduler.java
+++ src/main/java/co/fineants/stock/application/StockScheduler.java
@@ -11,7 +11,7 @@ import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 
 import co.fineants.api.domain.kis.service.CurrentPriceService;
 import co.fineants.stock.domain.event.StockReloadEvent;
-import co.fineants.stock.event.StockCurrentPriceRequiredEvent;
+import co.fineants.stock.event.StockCurrentPriceRefreshEvent;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -36,12 +36,12 @@ public class StockScheduler {
 	/**
 	 * 평일 15:30에 캐시 저장소에 저장된 종목의 현재가를 최신 정보로 갱신합니다.
 	 */
-	@SchedulerLock(name = "batchStockCurrentPriceRefreshScheduler", lockAtLeastFor = "1m", lockAtMostFor = "1m")
+	@SchedulerLock(name = "batchStockRefreshCurrentPriceScheduler", lockAtLeastFor = "1m", lockAtMostFor = "1m")
 	@Scheduled(cron = "0 30 15 ? * MON-FRI")
-	public void scheduledRefreshCurrentPrice() {
+	public void scheduledBatchRefreshCurrentPrice() {
 		// redis에 저장된 현재가 종목들을 가져옵니다.
 		Set<String> tickers = currentPriceService.getAllTickers();
-		// 각 종목의 현재가를 갱신하는 동기 이벤트를 발행합니다.
-		tickers.forEach(ticker -> publisher.publishEvent(new StockCurrentPriceRequiredEvent(ticker)));
+		// 각 종목의 현재가를 갱신하는 비동기 이벤트를 발행합니다.
+		tickers.forEach(ticker -> publisher.publishEvent(new StockCurrentPriceRefreshEvent(ticker)));
 	}
 }
