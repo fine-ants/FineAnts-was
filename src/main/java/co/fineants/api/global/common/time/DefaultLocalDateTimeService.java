0c9aba42392fe3d5c28bc1b457e09850d4d6f1fa
yonghwankim-dev <dragonbead95@naver.com>
2026-02-14 14:17:46 +0900

    move @Primary in LocalDateTimeService

--- src/main/java/co/fineants/api/global/common/time/DefaultLocalDateTimeService.java
+++ src/main/java/co/fineants/api/global/common/time/DefaultLocalDateTimeService.java
@@ -1,7 +1,9 @@
 package co.fineants.api.global.common.time;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class DefaultLocalDateTimeService implements LocalDateTimeService {
 }
--- src/main/java/co/fineants/api/global/common/time/FixedLocalDateTimeService.java
+++ src/main/java/co/fineants/api/global/common/time/FixedLocalDateTimeService.java
@@ -3,11 +3,9 @@ package co.fineants.api.global.common.time;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
-@Primary
 public class FixedLocalDateTimeService implements LocalDateTimeService {
 
 	private final LocalDateTime fixedDateTime;
