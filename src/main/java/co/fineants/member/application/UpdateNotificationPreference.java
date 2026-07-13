48f49602f3549a48925872fff267e8deae515275
yonghwankim-dev <dragonbead95@naver.com>
2026-07-13 14:24:40 +0900

    fix: delete Listener test

--- src/main/java/co/fineants/member/application/UpdateNotificationPreference.java
+++ src/main/java/co/fineants/member/application/UpdateNotificationPreference.java
@@ -1,9 +1,10 @@
 package co.fineants.member.application;
 
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import co.fineants.api.domain.fcm.domain.dto.response.FcmDeleteResponse;
+import co.fineants.api.domain.fcm.service.FcmService;
 import co.fineants.api.global.common.authorized.Authorized;
 import co.fineants.api.global.common.authorized.service.MemberAuthorizedService;
 import co.fineants.api.global.common.resource.ResourceId;
@@ -11,7 +12,6 @@ import co.fineants.api.global.errors.exception.business.MemberNotFoundException;
 import co.fineants.member.domain.Member;
 import co.fineants.member.domain.MemberRepository;
 import co.fineants.member.domain.NotificationPreference;
-import co.fineants.member.domain.event.NotificationPreferenceChangeEvent;
 import co.fineants.member.presentation.dto.request.MemberNotificationPreferenceRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 public class UpdateNotificationPreference {
 
 	private final MemberRepository memberRepository;
-	private final ApplicationEventPublisher eventPublisher;
+	private final FcmService fcmService;
 
 	@Transactional
 	@Authorized(serviceClass = MemberAuthorizedService.class)
@@ -34,6 +34,14 @@ public class UpdateNotificationPreference {
 		NotificationPreference preference = request.toEntity();
 		member.setNotificationPreference(preference);
 
-		eventPublisher.publishEvent(new NotificationPreferenceChangeEvent(memberId, request.fcmTokenId()));
+		deleteFcmToken(preference, request.fcmTokenId());
+	}
+
+	// 계정 알림 설정이 모두 비활성화인 경우 토큰을 삭제하는 기능
+	private void deleteFcmToken(NotificationPreference preference, Long fcmTokenId) {
+		if (preference.isAllInActive() && fcmTokenId != null) {
+			FcmDeleteResponse response = fcmService.deleteToken(fcmTokenId);
+			log.info("회원 알림 설정 전체 비활성화로 인한 결과 : {}", response);
+		}
 	}
 }
