1a74aff032a44e836abc5b18f42a5f28823f6343
yonghwankim-dev <dragonbead95@naver.com>
2026-07-20 13:33:24 +0900

    test - create HolidayMarketStatusCheckerRuleUnitTest

--- src/test/java/co/fineants/api/domain/holding/service/market_status_checker/HolidayMarketStatusCheckerRuleTest.java
+++ src/test/java/co/fineants/api/domain/holding/service/market_status_checker/HolidayMarketStatusCheckerRuleTest.java
@@ -6,7 +6,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,35 +25,12 @@ class HolidayMarketStatusCheckerRuleTest extends AbstractContainerBaseTest {
 	@Autowired
 	@Qualifier("holidayMarketStatusCheckerRule")
 	private MarketStatusCheckerRule rule;
-
 	@Autowired
 	private RedisTemplate<String, Object> redisTemplate;
 
 	@BeforeEach
 	void setUp() {
 		dateTime = LocalDateTime.of(2025, 6, 6, 9, 0);
-	}
-
-	@DisplayName("dateTime이 공휴일인 경우에는 false를 반환한다")
-	@Test
-	void isOpen_shouldReturnFalse_whenDateTimeIsHoliday() {
-		// given
-		Holiday holiday = Holiday.close(dateTime.toLocalDate());
-		repository.save(holiday);
-		// when
-		boolean isOpen = rule.isOpen(dateTime);
-		// then
-		assertThat(isOpen).isFalse();
-	}
-
-	@DisplayName("dateTime이 공휴일이 아닌 경우에는 true를 반환한다")
-	@Test
-	void isOpen_shouldReturnTrue_whenDateTimeIsNotHoliday() {
-		// given
-		// when
-		boolean isOpen = rule.isOpen(dateTime);
-		// then
-		assertThat(isOpen).isTrue();
 	}
 
 	@DisplayName("dateTime에 따른 공휴일 캐시가 존재하는 경우 캐시값을 반환한다")
@@ -71,17 +47,5 @@ class HolidayMarketStatusCheckerRuleTest extends AbstractContainerBaseTest {
 		boolean cachedIsOpen = Boolean.parseBoolean(
 			Objects.requireNonNull(redisTemplate.opsForValue().get("holidayMarketStatus::" + date)).toString());
 		assertThat(cachedIsOpen).isFalse();
-	}
-
-	@DisplayName("휴장일이 아니면 false를 반환한다")
-	@Test
-	void givenDateTime_whenNotHoliday_thenReturnTrue() {
-		// given
-		repository.save(Holiday.close(LocalDate.of(2025, 6, 6)));
-		LocalDateTime localDateTime = LocalDateTime.of(2025, 6, 6, 9, 0);
-		// when
-		boolean isOpen = rule.isOpen(localDateTime);
-		// then
-		Assertions.assertThat(isOpen).isFalse();
 	}
 }
---
+++ src/test/java/co/fineants/api/domain/holding/service/market_status_checker/HolidayMarketStatusCheckerRuleUnitTest.java
@@ -0,0 +1,58 @@
+package co.fineants.api.domain.holding.service.market_status_checker;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import co.fineants.api.domain.holiday.domain.entity.Holiday;
+import co.fineants.api.domain.holiday.repository.HolidayRepository;
+
+@ExtendWith(MockitoExtension.class)
+class HolidayMarketStatusCheckerRuleUnitTest {
+
+	@Mock
+	private HolidayRepository repository;
+	private LocalDateTime dateTime;
+	private MarketStatusCheckerRule rule;
+	private RedisTemplate<String, Object> redisTemplate;
+
+	@BeforeEach
+	void setUp() {
+		dateTime = LocalDateTime.of(2025, 6, 6, 9, 0);
+		rule = new HolidayMarketStatusCheckerRule(repository);
+	}
+
+	@DisplayName("dateTime이 공휴일인 경우에는 false를 반환한다")
+	@Test
+	void should_return_false_when_date_time_is_holiday() {
+		// given
+		BDDMockito.given(repository.findByBaseDate(dateTime.toLocalDate()))
+			.willReturn(Optional.of(Holiday.close(dateTime.toLocalDate())));
+		// when
+		boolean isOpen = rule.isOpen(dateTime);
+		// then
+		assertThat(isOpen).isFalse();
+	}
+
+	@DisplayName("dateTime이 공휴일이 아닌 경우에는 true를 반환한다")
+	@Test
+	void should_return_true_when_date_time_is_not_holiday() {
+		// given
+		BDDMockito.given(repository.findByBaseDate(dateTime.toLocalDate()))
+			.willReturn(Optional.empty());
+		// when
+		boolean isOpen = rule.isOpen(dateTime);
+		// then
+		assertThat(isOpen).isTrue();
+	}
+}
