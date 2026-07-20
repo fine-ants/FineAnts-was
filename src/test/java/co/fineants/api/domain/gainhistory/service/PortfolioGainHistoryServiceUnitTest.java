0c525013aad655501b4568c117011ed1b42b5237
yonghwankim-dev <dragonbead95@naver.com>
2026-07-20 11:16:56 +0900

    test - copy to PortfolioGainHistoryServiceUnitTest

---
+++ src/test/java/co/fineants/api/domain/gainhistory/service/PortfolioGainHistoryServiceUnitTest.java
@@ -0,0 +1,118 @@
+package co.fineants.api.domain.gainhistory.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+
+import co.fineants.AbstractContainerBaseTest;
+import co.fineants.api.domain.common.count.Count;
+import co.fineants.api.domain.common.money.Money;
+import co.fineants.api.domain.gainhistory.domain.dto.response.PortfolioGainHistoryCreateResponse;
+import co.fineants.api.domain.gainhistory.domain.entity.PortfolioGainHistory;
+import co.fineants.api.domain.gainhistory.repository.PortfolioGainHistoryRepository;
+import co.fineants.api.domain.holding.domain.entity.PortfolioHolding;
+import co.fineants.api.domain.holding.repository.PortfolioHoldingRepository;
+import co.fineants.api.domain.kis.repository.CurrentPriceRepository;
+import co.fineants.api.domain.portfolio.domain.entity.Portfolio;
+import co.fineants.api.domain.portfolio.repository.PortfolioRepository;
+import co.fineants.api.domain.purchasehistory.repository.PurchaseHistoryRepository;
+import co.fineants.member.domain.Member;
+import co.fineants.member.domain.MemberRepository;
+import co.fineants.stock.domain.Stock;
+import co.fineants.stock.domain.StockRepository;
+
+class PortfolioGainHistoryServiceUnitTest extends AbstractContainerBaseTest {
+
+	@Autowired
+	private PortfolioGainHistoryService service;
+
+	@Autowired
+	private PortfolioGainHistoryRepository portfolioGainHistoryRepository;
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Autowired
+	private PortfolioRepository portfolioRepository;
+
+	@Autowired
+	private StockRepository stockRepository;
+
+	@Autowired
+	private PortfolioHoldingRepository portFolioHoldingRepository;
+
+	@Autowired
+	private PurchaseHistoryRepository purchaseHistoryRepository;
+
+	@Autowired
+	private CurrentPriceRepository currentPriceRepository;
+
+	@Autowired
+	private CacheManager cacheManager;
+
+	@DisplayName("모든 포트폴리오의 손익 내역을 추가한다")
+	@Test
+	void addPortfolioGainHistory() {
+		// given
+		Member member = memberRepository.save(createMember());
+		Portfolio savePortfolio = portfolioRepository.save(createPortfolio(member));
+		Stock stock = stockRepository.save(createSamsungStock());
+		PortfolioHolding portfolioHolding = portFolioHoldingRepository.save(PortfolioHolding.of(savePortfolio, stock));
+
+		LocalDateTime purchaseDate = LocalDateTime.of(2023, 9, 26, 9, 30, 0);
+		Count numShares = Count.from(3);
+		Money purchasePricePerShare = Money.won(50000);
+		String memo = "첫구매";
+		purchaseHistoryRepository.save(
+			createPurchaseHistory(null, purchaseDate, numShares, purchasePricePerShare, memo, portfolioHolding));
+		currentPriceRepository.savePrice(stock, 60_000);
+
+		// when
+		PortfolioGainHistoryCreateResponse response = service.addPortfolioGainHistory();
+
+		// then
+		assertAll(
+			() -> assertThat(response).extracting("ids")
+				.asList()
+				.hasSize(1),
+			() -> assertThat(portfolioGainHistoryRepository.findAll())
+				.extracting(PortfolioGainHistory::getTotalGain, PortfolioGainHistory::getDailyGain,
+					PortfolioGainHistory::getCurrentValuation)
+				.usingComparatorForType(Money::compareTo, Money.class)
+				.containsExactlyInAnyOrder(Tuple.tuple(Money.won(30000L), Money.won(30000L), Money.won(180000L)))
+		);
+	}
+
+	@DisplayName("포트폴리오 손익 내역 추가시 캐시를 제거한다")
+	@Test
+	void givenPortfolios_whenAddPortfolioGainHistory_thenDeleteCache() {
+		// given
+		Member member = memberRepository.save(createMember());
+		Portfolio portfolio = portfolioRepository.save(createPortfolio(member));
+		Stock stock = stockRepository.save(createSamsungStock());
+		PortfolioHolding portfolioHolding = portFolioHoldingRepository.save(PortfolioHolding.of(portfolio, stock));
+
+		LocalDateTime purchaseDate = LocalDateTime.of(2023, 9, 26, 9, 30, 0);
+		Count numShares = Count.from(3);
+		Money purchasePricePerShare = Money.won(50000);
+		String memo = "첫구매";
+		purchaseHistoryRepository.save(
+			createPurchaseHistory(null, purchaseDate, numShares, purchasePricePerShare, memo, portfolioHolding));
+		currentPriceRepository.savePrice(stock, 60_000);
+
+		// when
+		PortfolioGainHistoryCreateResponse response = service.addPortfolioGainHistory();
+
+		// then
+		assertThat(response).extracting("ids").asList().hasSize(1);
+		assertThat(Objects.requireNonNull(cacheManager.getCache("lineChartCache")).get(portfolio.getId())).isNull();
+	}
+}
