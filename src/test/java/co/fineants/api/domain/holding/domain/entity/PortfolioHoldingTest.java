b2c426ab406dec8b7c97589f85230065e7e34c45
yonghwankim-dev <dragonbead95@naver.com>
2026-07-20 12:57:07 +0900

    test - convert to unit test with PortfolioHoldingTest

--- src/test/java/co/fineants/api/domain/holding/domain/entity/PortfolioHoldingTest.java
+++ src/test/java/co/fineants/api/domain/holding/domain/entity/PortfolioHoldingTest.java
@@ -4,30 +4,30 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import co.fineants.AbstractContainerBaseTest;
-import co.fineants.member.domain.Member;
+import co.fineants.TestDataFactory;
 import co.fineants.api.domain.portfolio.domain.entity.Portfolio;
+import co.fineants.member.domain.Member;
 import co.fineants.stock.domain.Stock;
 
-class PortfolioHoldingTest extends AbstractContainerBaseTest {
+class PortfolioHoldingTest {
 
 	@DisplayName("포트폴리오 종목에 포트폴리오를 설정한다")
 	@Test
-	void setPortfolio() {
+	void should_set_other_portfolio() {
 		// given
-		Member member = createMember();
-		Portfolio portfolio = createPortfolio(member);
-		Stock stock = createSamsungStock();
-		PortfolioHolding holding = createPortfolioHolding(portfolio, stock);
+		Member member = TestDataFactory.createMember();
+		Portfolio portfolio = TestDataFactory.createPortfolio(member);
+		Stock stock = TestDataFactory.createSamsungStock();
+		PortfolioHolding holding = TestDataFactory.createPortfolioHolding(portfolio, stock);
 
-		Portfolio other = createPortfolio(member, "other");
+		Portfolio other = TestDataFactory.createPortfolio(member, "other");
 		// when
 		holding.setPortfolio(other);
 		// then
 		Assertions.assertThat(portfolio.getPortfolioHoldings()).isEmpty();
 		Assertions.assertThat(other.getPortfolioHoldings())
 			.hasSize(1)
-			.containsExactlyInAnyOrder(createPortfolioHolding(other, stock));
+			.containsExactlyInAnyOrder(TestDataFactory.createPortfolioHolding(other, stock));
 		Assertions.assertThat(holding.getPortfolio()).isEqualTo(other);
 	}
 }
