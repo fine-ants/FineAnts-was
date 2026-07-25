e7081a5365a82709874d50eb695e75dbea2b69b3
yonghwankim-dev <dragonbead95@naver.com>
2026-07-25 12:10:50 +0900

    test - delete test code

--- src/test/java/co/fineants/api/domain/kis/service/CurrentPriceServiceUnitTest.java
+++ src/test/java/co/fineants/api/domain/kis/service/CurrentPriceServiceUnitTest.java
@@ -23,13 +23,11 @@ import co.fineants.api.domain.common.money.Money;
 import co.fineants.api.domain.holding.service.market_status_checker.MarketStatusChecker;
 import co.fineants.api.domain.holiday.domain.entity.Holiday;
 import co.fineants.api.domain.holiday.service.HolidayService;
-import co.fineants.api.domain.kis.client.KisCurrentPrice;
 import co.fineants.api.domain.kis.domain.CurrentPriceRedisEntity;
 import co.fineants.api.domain.kis.repository.CurrentPriceRepository;
 import co.fineants.api.global.common.time.LocalDateTimeService;
 import co.fineants.stock.event.StockCurrentPriceRefreshEvent;
 import co.fineants.stock.event.StockCurrentPriceRequiredEvent;
-import reactor.core.publisher.Mono;
 
 @ExtendWith(MockitoExtension.class)
 class CurrentPriceServiceUnitTest {
@@ -144,57 +142,6 @@ class CurrentPriceServiceUnitTest {
 		BDDMockito.verify(eventPublisher, Mockito.times(1))
 			.publishEvent(new StockCurrentPriceRefreshEvent(tickerSymbol));
 		Assertions.assertThat(actualPrice).isEqualTo(Money.won(stalePrice));
-	}
-
-	@DisplayName("종목 현재가 조회 - 외부 API 호출 실패 시 예외를 던진다")
-	@Test
-	void fetchPrice_whenExternalAPIFails_thenThrowException() {
-		// given
-		String tickerSymbol = "005930";
-		BDDMockito.given(kisService.fetchCurrentPrice(tickerSymbol))
-			.willReturn(Mono.empty());
-
-		// when
-		Throwable throwable = Assertions.catchThrowable(() -> service.fetchPrice(tickerSymbol));
-		// then
-		Assertions.assertThat(throwable)
-			.isInstanceOf(IllegalStateException.class)
-			.hasMessage("Failed to fetch current price for " + tickerSymbol);
-	}
-
-	@DisplayName("종목 현재가 조회 - 상장 폐지된 종목을 대상으로 조회시 0원을 반환한다")
-	@Test
-	void fetchPrice_whenDelistedStock_thenReturnZeroWon() {
-		// given
-		String tickerSymbol = "999999"; // 상장 폐지된 종목
-		BDDMockito.given(kisService.fetchCurrentPrice(tickerSymbol))
-			.willReturn(Mono.just(KisCurrentPrice.empty(tickerSymbol)));
-
-		// when
-		Money actualPrice = service.fetchPrice(tickerSymbol);
-
-		// then
-		Assertions.assertThat(actualPrice).isEqualTo(Money.won(0L));
-		CurrentPriceRedisEntity actual = currentPriceRepository.fetchPriceBy(tickerSymbol).orElseThrow();
-		Assertions.assertThat(actual)
-			.hasFieldOrPropertyWithValue("tickerSymbol", tickerSymbol)
-			.hasFieldOrPropertyWithValue("price", 0L);
-	}
-
-	@DisplayName("종목 현재가 조회 - 장시간 외에서 데이터가 존재하지 않으면 동기 이벤트로 조회후 반환한다")
-	@Test
-	void fetchPrice_whenMarketIsCloseAndCurrentPriceIsNotExist_thenPublishEventAndReturnCurrentPrice() {
-		// given
-		String tickerSymbol = "005930";
-		long freshPrice = 50000L;
-		BDDMockito.given(kisService.fetchCurrentPrice(tickerSymbol))
-			.willReturn(Mono.just(KisCurrentPrice.create(tickerSymbol, freshPrice)));
-
-		// when
-		Money actualPrice = service.fetchPrice(tickerSymbol);
-
-		// then
-		Assertions.assertThat(actualPrice).isEqualTo(Money.won(freshPrice));
 	}
 
 	@DisplayName("종목 현재가 조회 - 장시간 외에서 신선한 데이터가 존재하면 해당 데이터를 반환한다")
