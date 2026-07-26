28764b3ced932dcf2ebdce8c9f8e90c29ae3e3bc
yonghwankim-dev <dragonbead95@naver.com>
2026-07-26 14:23:47 +0900

    feat- create KisAccessTokenRedisRepository

---
+++ src/main/java/co/fineants/api/domain/kis/repository/infrastructure/KisAccessTokenRedisRepository.java
@@ -0,0 +1,70 @@
+package co.fineants.api.domain.kis.repository.infrastructure;
+
+import static co.fineants.api.domain.kis.service.KisAccessTokenRedisService.*;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.data.redis.RedisSystemException;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import co.fineants.api.domain.kis.client.KisAccessToken;
+import co.fineants.api.domain.kis.domain.repository.KisAccessTokenRepository;
+import co.fineants.api.global.util.ObjectMapperUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Repository
+@RequiredArgsConstructor
+@Slf4j
+public class KisAccessTokenRedisRepository implements KisAccessTokenRepository {
+
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	@Override
+	public void save(KisAccessToken accessToken) {
+		// todo: 매개변수화
+		LocalDateTime now = LocalDateTime.now();
+		try {
+			redisTemplate.opsForValue().set(ACCESS_TOKEN_MAP_KEY,
+				ObjectMapperUtil.serialize(accessToken),
+				accessToken.betweenSecondFrom(now));
+		} catch (RedisSystemException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public Optional<KisAccessToken> get() {
+		Object result = redisTemplate.opsForValue().get(ACCESS_TOKEN_MAP_KEY);
+		if (result == null) {
+			return Optional.empty();
+		}
+		return Optional.of(ObjectMapperUtil.deserialize((String)result, KisAccessToken.class));
+	}
+
+	@Override
+	public boolean isAccessTokenExpired(LocalDateTime dateTime) {
+		// todo: service로 이전할 예정
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String createAuthorization() {
+		// todo: service로 이전할 예정
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean isTokenExpiringSoon(LocalDateTime localDateTime) {
+		// todo: service로 이전할 예정
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		Boolean isDeleted = redisTemplate.delete(ACCESS_TOKEN_MAP_KEY);
+		log.info("액세스 토큰 제거 완료 여부 : {}", isDeleted);
+	}
+}
