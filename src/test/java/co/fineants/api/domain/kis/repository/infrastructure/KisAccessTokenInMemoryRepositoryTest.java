057255a97327ca9e03da29e1df1ee31a032b920e
yonghwankim-dev <dragonbead95@naver.com>
2026-07-26 13:46:27 +0900

    test - add test, createAuthorization

--- src/test/java/co/fineants/api/domain/kis/repository/infrastructure/KisAccessTokenInMemoryRepositoryTest.java
+++ src/test/java/co/fineants/api/domain/kis/repository/infrastructure/KisAccessTokenInMemoryRepositoryTest.java
@@ -33,6 +33,16 @@ class KisAccessTokenInMemoryRepositoryTest {
 			.doesNotThrowAnyException();
 	}
 
+	@DisplayName("액세스 토큰 저장 - 액세스 토큰이 null이어도 예외가 발생하지 않는다")
+	@Test
+	void should_does_not_throw_exception_when_access_token_is_null() {
+		// given
+		KisAccessToken kisAccessToken = null;
+		// when & then
+		Assertions.assertThatCode(() -> repository.save(kisAccessToken))
+			.doesNotThrowAnyException();
+	}
+
 	@DisplayName("액세스 토큰 조회")
 	@Test
 	void should_return_access_token() {
@@ -67,5 +77,42 @@ class KisAccessTokenInMemoryRepositoryTest {
 		boolean actual = repository.isAccessTokenExpired(expiredDateTime);
 		// then
 		Assertions.assertThat(actual).isTrue();
+	}
+
+	@DisplayName("액세스 토큰 만료 여부 - 액세스 토큰이 저장되어 있지 않으면 true를 반환한다")
+	@Test
+	void should_return_true_when_not_saved_access_token() {
+		// given
+		LocalDateTime baseTime = LocalDate.of(2023, 12, 23).atStartOfDay();
+		LocalDateTime expiredDateTime = baseTime.plusHours(24);
+		// when
+		boolean actual = repository.isAccessTokenExpired(expiredDateTime);
+		// then
+		Assertions.assertThat(actual).isTrue();
+	}
+
+	@DisplayName("액세스 토큰 만료 여부 - 시간이 만료시간보다 같거나 작으면 false를 반환해야 한다")
+	@Test
+	void should_return_false_when_date_time_is_equal_less_than_base_time() {
+		// given
+		LocalDateTime baseTime = LocalDate.of(2023, 12, 23).atStartOfDay();
+		KisAccessToken accessToken = TestDataFactory.createKisAccessToken(baseTime);
+		repository.save(accessToken);
+		// when & then
+		Assertions.assertThat(repository.isAccessTokenExpired(baseTime.plusHours(24))).isFalse();
+		Assertions.assertThat(repository.isAccessTokenExpired(baseTime.plusHours(24).minusSeconds(1))).isFalse();
+	}
+
+	@DisplayName("인증 헤더 문자열 생성")
+	@Test
+	void should_return_authorization() {
+		// given
+		LocalDateTime baseTime = LocalDate.of(2023, 12, 23).atStartOfDay();
+		KisAccessToken accessToken = TestDataFactory.createKisAccessToken(baseTime);
+		repository.save(accessToken);
+		// when
+		String authorization = repository.createAuthorization();
+		// then
+		Assertions.assertThat(authorization).isEqualTo("Bearer accessToken");
 	}
 }
