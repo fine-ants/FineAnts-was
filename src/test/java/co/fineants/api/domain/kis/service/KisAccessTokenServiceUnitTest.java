30a5b7290e685b5ce03dae8bfe2776bf8a00f3ea
yonghwankim-dev <dragonbead95@naver.com>
2026-07-27 10:40:16 +0900

    test - fix KisAccessTokenServiceUnitTest
    의존 객체가 repository로 변경됨에 따라서 테스트코드도 변경됨

--- src/test/java/co/fineants/api/domain/kis/service/KisAccessTokenServiceUnitTest.java
+++ src/test/java/co/fineants/api/domain/kis/service/KisAccessTokenServiceUnitTest.java
@@ -1,14 +1,10 @@
 package co.fineants.api.domain.kis.service;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,14 +12,10 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.redis.RedisSystemException;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 
 import co.fineants.TestDataFactory;
 import co.fineants.api.domain.kis.client.KisAccessToken;
-import co.fineants.api.domain.kis.repository.infrastructure.KisAccessTokenRedisRepository;
-import co.fineants.api.global.util.ObjectMapperUtil;
+import co.fineants.api.domain.kis.domain.repository.KisAccessTokenRepository;
 
 @ExtendWith(MockitoExtension.class)
 class KisAccessTokenServiceUnitTest {
@@ -32,16 +24,7 @@ class KisAccessTokenServiceUnitTest {
 	private KisAccessTokenService service;
 
 	@Mock
-	private RedisTemplate<String, Object> redisTemplate;
-
-	@Mock
-	private ValueOperations<String, Object> valueOperations;
-
-	@BeforeEach
-	void setUp() {
-		BDDMockito.given(redisTemplate.opsForValue())
-			.willReturn(valueOperations);
-	}
+	private KisAccessTokenRepository repository;
 
 	@DisplayName("액세스 토큰 저장 - kis 액세스 토큰맵을 저장한다")
 	@Test
@@ -57,18 +40,15 @@ class KisAccessTokenServiceUnitTest {
 			.doesNotThrowAnyException();
 	}
 
-	@DisplayName("이미 만료된 액세스 토큰을 redis에 저장할 수 없다.")
+	@DisplayName("이미 만료된 액세스 토큰을 저장할 수 없다.")
 	@Test
 	void should_not_set_access_token_map_when_expired_access_token() {
 		// given
 		LocalDateTime createdAt = LocalDate.of(2026, 7, 26).atStartOfDay();
 		KisAccessToken accessToken = TestDataFactory.createKisAccessToken(createdAt);
 
-		LocalDateTime now = createdAt.plusSeconds(accessToken.getExpiresIn());
-		Duration timeout = Duration.ofSeconds(accessToken.betweenSecondFrom(now).toSeconds());
-		BDDMockito.willThrow(RedisSystemException.class)
-			.given(valueOperations)
-			.set(KisAccessTokenRedisRepository.ACCESS_TOKEN_MAP_KEY, ObjectMapperUtil.serialize(accessToken), timeout);
+		LocalDateTime now = createdAt.plusSeconds(accessToken.getExpiresIn())
+			.plusSeconds(1);
 
 		// when & then
 		Assertions.assertThatCode(() -> service.setAccessTokenMap(accessToken, now))
@@ -81,9 +61,9 @@ class KisAccessTokenServiceUnitTest {
 		// given
 		LocalDateTime createdAt = LocalDate.of(2026, 7, 26).atStartOfDay();
 		KisAccessToken accessToken = TestDataFactory.createKisAccessToken(createdAt);
-		String json = ObjectMapperUtil.serialize(accessToken);
-		BDDMockito.given(valueOperations.get(KisAccessTokenRedisRepository.ACCESS_TOKEN_MAP_KEY))
-			.willReturn(json);
+
+		BDDMockito.given(repository.get())
+			.willReturn(Optional.of(accessToken));
 		// when
 		Optional<KisAccessToken> actual = service.getAccessTokenMap();
 
@@ -94,15 +74,15 @@ class KisAccessTokenServiceUnitTest {
 		Assertions.assertThat(actual).contains(expected);
 	}
 
-	@DisplayName("Redis에 accessToken이 없는 경우 Optional.empty()를 반환한다")
+	@DisplayName("저장소에 accessToken이 없는 경우 Optional.empty()를 반환한다")
 	@Test
 	void should_return_empty_optional_when_get_access_token() {
 		// given
-		BDDMockito.given(valueOperations.get(KisAccessTokenRedisRepository.ACCESS_TOKEN_MAP_KEY))
-			.willReturn(null);
+		BDDMockito.given(repository.get())
+			.willReturn(Optional.empty());
 		// when
 		Optional<KisAccessToken> optionalKisAccessToken = service.getAccessTokenMap();
 		// then
-		assertThat(optionalKisAccessToken).isEmpty();
+		Assertions.assertThat(optionalKisAccessToken).isEmpty();
 	}
 }
